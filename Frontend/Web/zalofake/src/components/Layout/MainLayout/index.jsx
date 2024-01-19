8033ce63de7d7c67c5157eeead62442f03d00b65
Bùi Trí Thức <20088361.thuc@student.iuh.edu.vn>
2024-01-19 23:29:14 +0700

    Add UI : setting and profile menu. Update UI of Mobile

--- Frontend/Web/zalofake/src/components/Layout/MainLayout/index.jsx
+++ Frontend/Web/zalofake/src/components/Layout/MainLayout/index.jsx
@@ -1,25 +1,57 @@
 import Sidebar from "../../SidebarComponent";
 import { useState } from "react";
+import MenuComponent from "../../MenuComponent";
+import ModalComponent from "../../ModalComponent/insex";
 
-function MainLayout({isLogin}) {
-
+function MainLayout({ isLogin }) {
   //check login
   // if (!isLogin) {
   //   window.location.href = "/";
   // }
-  const [isShowSetting, setisShowSetting] = useState(false)
+  const [showModal, setShowModal] = useState(true);
+  const [curentTab, setCurentTab] = useState();
+  const [language, setLanguage] = useState("vi");
+
+  const showModalProfile = () => {
+    setShowModal(!showModal);
+    setCurentTab()
+  };
+
+  const changeTab = (tab) => {
+    setCurentTab(tab);
+  };
+
   return (
     <>
       <div className="w-screen h-screen flex">
         <div className="h-screen">
-          <Sidebar isShowSetting={isShowSetting} curentTab={0}/>
+          <Sidebar curentTab={curentTab} changeTab={changeTab} />
         </div>
-        <div className="h-screen w-full md:w-96">
-          {
-            isShowSetting ? <SettingComponent /> : <></>
-          }
+        <div className="h-screen w-full md:w-96 relative">
+          {curentTab == 5 ? (
+            <MenuComponent
+              language={language}
+              setLanguage={setLanguage}
+              typeMenu={"setting"}
+              showModal={showModalProfile}
+            />
+          ) : curentTab == 0 ?(
+            <MenuComponent
+              language={language}
+              setLanguage={setLanguage}
+              typeMenu={"profile"}
+              showModal={showModalProfile}
+
+            />
+          ) : (
+            <></>
+          )}
         </div>
       </div>
+
+      {showModal ? (
+        <ModalComponent showModal={showModalProfile} language={language}/>
+      ) : null}
     </>
   );
 }
