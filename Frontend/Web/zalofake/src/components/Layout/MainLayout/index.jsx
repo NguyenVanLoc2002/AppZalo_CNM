0aa0a3c16c6c5df7199ab038b130df61c58f543d
Bùi Trí Thức <20088361.thuc@student.iuh.edu.vn>
2024-01-20 15:02:04 +0700

    Update Web UI : Mainlayout component, Setting component...

--- Frontend/Web/zalofake/src/components/Layout/MainLayout/index.jsx
+++ Frontend/Web/zalofake/src/components/Layout/MainLayout/index.jsx
@@ -2,56 +2,158 @@ import Sidebar from "../../SidebarComponent";
 import { useState } from "react";
 import MenuComponent from "../../MenuComponent";
 import ModalComponent from "../../ModalComponent/insex";
+import ContactComponent from "../../ContactComponent";
 
 function MainLayout({ isLogin }) {
-  //check login
-  // if (!isLogin) {
-  //   window.location.href = "/";
-  // }
-  const [showModal, setShowModal] = useState(true);
+  
+  const [showModal, setShowModal] = useState(false);
   const [curentTab, setCurentTab] = useState();
+  const [currentMenu, setCurrentMenu] = useState();
   const [language, setLanguage] = useState("vi");
 
   const showModalProfile = () => {
     setShowModal(!showModal);
-    setCurentTab()
+    setCurentTab();
   };
 
   const changeTab = (tab) => {
     setCurentTab(tab);
   };
 
+  const changeMenu = (menu) => {
+    if (currentMenu == menu) {
+      setCurrentMenu();
+    } else {
+      setCurrentMenu(menu);
+    }
+  };
+
   return (
     <>
       <div className="w-screen h-screen flex">
         <div className="h-screen">
-          <Sidebar curentTab={curentTab} changeTab={changeTab} />
+          <Sidebar
+            curentTab={curentTab}
+            changeTab={changeTab}
+            changeMenu={changeMenu}
+          />
         </div>
-        <div className="h-screen w-full md:w-96 relative">
-          {curentTab == 5 ? (
-            <MenuComponent
-              language={language}
-              setLanguage={setLanguage}
-              typeMenu={"setting"}
-              showModal={showModalProfile}
-            />
-          ) : curentTab == 0 ?(
-            <MenuComponent
-              language={language}
-              setLanguage={setLanguage}
-              typeMenu={"profile"}
-              showModal={showModalProfile}
+        <div className="h-screen w-full relative">
+          {curentTab == 1 ? (
+            <>
+              {/* Goi toi cac component tương ung voi tab cho nay ne nha :V  */}
+              {currentMenu == 1 ? (
+                <MenuComponent
+                  language={language}
+                  setLanguage={setLanguage}
+                  typeMenu={"profile"}
+                  showModal={showModalProfile}
+                />
+              ) : currentMenu == 2 ? (
+                <MenuComponent
+                  language={language}
+                  setLanguage={setLanguage}
+                  typeMenu={"setting"}
+                  showModal={showModalProfile}
+                />
+              ) : (
+                <> </>
+              )}
+            </>
+          ) : curentTab == 2 ? (
+            <>
+              <ContactComponent />
+              {currentMenu == 1 ? (
+                <MenuComponent
+                  language={language}
+                  setLanguage={setLanguage}
+                  typeMenu={"profile"}
+                  showModal={showModalProfile}
+                />
+              ) : currentMenu == 2 ? (
+                <MenuComponent
+                  language={language}
+                  setLanguage={setLanguage}
+                  typeMenu={"setting"}
+                  showModal={showModalProfile}
+                />
+              ) : (
+                <> </>
+              )}
+            </>
+          ) : curentTab == 3 ? (
+            <>
+              {/* Goi toi cac component tương ung voi tab cho nay ne nha  :V  */}
 
-            />
+              {currentMenu == 1 ? (
+                <MenuComponent
+                  language={language}
+                  setLanguage={setLanguage}
+                  typeMenu={"profile"}
+                  showModal={showModalProfile}
+                />
+              ) : currentMenu == 2 ? (
+                <MenuComponent
+                  language={language}
+                  setLanguage={setLanguage}
+                  typeMenu={"setting"}
+                  showModal={showModalProfile}
+                />
+              ) : (
+                <> </>
+              )}
+            </>
+          ) : curentTab == 4 ? (
+            <>
+              {/* Goi toi cac component tương ung voi tab cho nay ne nha  :V  */}
+
+              {currentMenu == 1 ? (
+                <MenuComponent
+                  language={language}
+                  setLanguage={setLanguage}
+                  typeMenu={"profile"}
+                  showModal={showModalProfile}
+                />
+              ) : currentMenu == 2 ? (
+                <MenuComponent
+                  language={language}
+                  setLanguage={setLanguage}
+                  typeMenu={"setting"}
+                  showModal={showModalProfile}
+                />
+              ) : (
+                <> </>
+              )}
+            </>
           ) : (
-            <></>
+            <>
+              {/* Goi toi cac component tương ung voi tab cho nay ne nha  :V  */}
+
+              {currentMenu == 1 ? (
+                <MenuComponent
+                  language={language}
+                  setLanguage={setLanguage}
+                  typeMenu={"profile"}
+                  showModal={showModalProfile}
+                />
+              ) : currentMenu == 2 ? (
+                <MenuComponent
+                  language={language}
+                  setLanguage={setLanguage}
+                  typeMenu={"setting"}
+                  showModal={showModalProfile}
+                />
+              ) : (
+                <> </>
+              )}
+            </>
           )}
         </div>
       </div>
 
       {showModal ? (
-        <ModalComponent showModal={showModalProfile} language={language}/>
-      ) : null}
+        <ModalComponent showModal={showModalProfile} language={language} />
+      ) : <> </> }
     </>
   );
 }
