732b0fa3f150eedef572cdb6f7e289317b90b66b
BaoTruc <tranbaotruc2002bc@gmail.com>
2024-01-24 19:06:24 +0700

    Add component directory friends  and 6 screen

--- Frontend/Mobile/zalofake/App.js
+++ Frontend/Mobile/zalofake/App.js
@@ -10,6 +10,7 @@ import CreateAccount from "./src/LoginComponents/CreateAccount"
 import CreateAccount1 from "./src/LoginComponents/CreateAccount1"
 import EnterAuthCode from "./src/LoginComponents/EnterAuthCode"
 import Login from "./src/LoginComponents/Login"
+import FriendDirectory from "./src/DirectoryComponents/FriendDirectory"
 
 const Stack = createNativeStackNavigator();
 
@@ -22,40 +23,43 @@ export default function App() {
         />        
         <Stack.Screen name="ChatContent" component={Chat} />       
       </Stack.Navigator> */}
-      <Stack.Navigator initialRouteName="LoginMain">
-        <Stack.Screen name="LoginMain" component={LoginMain} options={{ headerShown: false }}/>     
+      <Stack.Navigator initialRouteName="FriendDirectory">
+        <Stack.Screen name="LoginMain" component={LoginMain} options={{ headerShown: false }} />
         <Stack.Screen name="CreateAccount" component={CreateAccount} options={{
           headerStyle: {
-            backgroundColor: '#0091FF', 
+            backgroundColor: '#0091FF',
           },
           headerTintColor: '#fff',
-          title:'Tạo tài khoản'
-        }}/>   
+          title: 'Tạo tài khoản'
+        }} />
         <Stack.Screen name="CreateAccount1" component={CreateAccount1} options={{
           headerStyle: {
-            backgroundColor: '#0091FF', 
-            
+            backgroundColor: '#0091FF',
+
           },
-          headerTintColor: '#fff', 
-          title:'Tạo tài khoản'
-        }}/>    
+          headerTintColor: '#fff',
+          title: 'Tạo tài khoản'
+        }} />
         <Stack.Screen name="EnterAuthCode" component={EnterAuthCode} options={{
           headerStyle: {
-            backgroundColor: '#0091FF', 
-            
+            backgroundColor: '#0091FF',
+
           },
-          headerTintColor: '#fff', 
-          title:'Nhập mã xác thực'
-        }}/>   
+          headerTintColor: '#fff',
+          title: 'Nhập mã xác thực'
+        }} />
         <Stack.Screen name="Login" component={Login} options={{
           headerStyle: {
-            backgroundColor: '#0091FF', 
-            
+            backgroundColor: '#0091FF',
+
           },
-          headerTintColor: '#fff', 
-          title:'Đăng nhập'
-        }}/>  
-           
+          headerTintColor: '#fff',
+          title: 'Đăng nhập'
+        }} />
+
+        <Stack.Screen name="FriendDirectory" component={FriendDirectory} 
+        options={{ headerShown: false }} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
