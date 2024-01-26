be97843564463d8558416f29b6acbeb793bb9f18
BaoTruc <tranbaotruc2002bc@gmail.com>
2024-01-27 00:35:01 +0700

    9 Screen _ Lan 1

--- Frontend/Mobile/zalofake/App.js
+++ Frontend/Mobile/zalofake/App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Ionicons } from "@expo/vector-icons";
 
 import "./style.css";
 
@@ -11,19 +12,65 @@ import CreateAccount1 from "./src/LoginComponents/CreateAccount1"
 import EnterAuthCode from "./src/LoginComponents/EnterAuthCode"
 import Login from "./src/LoginComponents/Login"
 import FriendDirectory from "./src/DirectoryComponents/FriendDirectory"
-
+import GroupDirectory from "./src/DirectoryComponents/GroupDirectory"
+import QA from "./src/DirectoryComponents/QA"
+import DirectoryComponents from "./src/DirectoryComponents/DirectoryComponents"
+import { Pressable, Image, Text } from "react-native";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+      {/* Screen Chat */}
+
       {/* <Stack.Navigator initialRouteName="ChatComponent">
         <Stack.Screen name="ChatComponent" component={ChatComponent}
         options={{ headerShown: false }}
         />        
         <Stack.Screen name="ChatContent" component={Chat} />       
       </Stack.Navigator> */}
-      <Stack.Navigator initialRouteName="FriendDirectory">
+
+      {/* 3 Screen Danh bạ */}
+
+      {/* <Stack.Navigator initialRouteName="DirectoryComponents">
+        <Stack.Screen name="DirectoryComponents" component={DirectoryComponents}
+          options={{
+            headerStyle: { backgroundColor: '#0091FF' },
+            title: 'Tìm kiếm',
+            headerTitleAlign: 'left',
+            headerTitleStyle: {
+              fontSize: 16,
+              color: '#fff'
+            },
+            headerLeft: () => (
+              <Pressable
+                className = {"ml-2"}
+                onPress={() => {
+                  // Xử lý sự kiện khi nhấn vào nút search
+                }}> <Ionicons name={"search-sharp"} size={30} color={"#fff"} />
+              </Pressable>
+
+            ),
+            headerRight: () => (
+              <Pressable
+              className = {"mr-2"}
+                onPress={() => {
+                  // Xử lý sự kiện khi nhấn vào add
+                }}> <Ionicons name={"person-add"} size={30} color={"#fff"} />
+              </Pressable>
+
+            ),
+            
+           
+
+          }}
+        />
+
+      </Stack.Navigator> */}
+
+      {/* 6 Screen Login */}
+
+      <Stack.Navigator initialRouteName="LoginMain">
         <Stack.Screen name="LoginMain" component={LoginMain} options={{ headerShown: false }} />
         <Stack.Screen name="CreateAccount" component={CreateAccount} options={{
           headerStyle: {
@@ -57,10 +104,10 @@ export default function App() {
           title: 'Đăng nhập'
         }} />
 
-        <Stack.Screen name="FriendDirectory" component={FriendDirectory} 
-        options={{ headerShown: false }} />
+       
 
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
