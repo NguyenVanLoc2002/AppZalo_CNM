494d1f8ad306d4850dbb7497bfc90fd235ce9702
Bùi Trí Thức <20088361.thuc@student.iuh.edu.vn>
2024-01-31 10:58:20 +0700

    Update UI : change sytle from tailwindcss to styleSheet css

--- Frontend/Mobile/zalofake/src/components/ChatComponent.js
+++ Frontend/Mobile/zalofake/src/components/ChatComponent.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
-import { View, Text, TextInput } from "react-native";
 
 import Chat from "./chat/Chat";
-import Contact from "./Contact";
 import Explore from "./Explore";
 import News from "./News";
 import FriendProfile from "./friendProfile/FriendProfile";
+import DirectoryComponents from "../DirectoryComponents/DirectoryComponents";
 
 const Tab = createBottomTabNavigator();
 
@@ -35,29 +34,11 @@ const ChatComponent = () => {
               : "information-circle-outline";
           }
           return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        headerTitle: () => (
-          <View style={{ flex: 1, flexDirection: "row", alignItems: "center" }}>
-             <Ionicons name="search" size={24} color="white" onPress={() => console.log("Search pressed")} />
-            <TextInput placeholder="Tìm kiếm" style={{ flex: 1, color: "white" , marginLeft:20}} />
-          </View>
-        ),
-        headerRight: () => (
-          <View style={{ flexDirection: "row", marginRight:10 , justifyContent: 'center', alignItems: 'center'}}>
-            <Ionicons name="qr-code" size={24} color="white" onPress={() => console.log("QR pressed")} />
-             <View style={{marginLeft:10}}>
-             <Ionicons name="add" size={35} color="white" onPress={() => console.log("Add friend pressed")} />
-             </View>
-          </View>
-        ),
-        headerStyle: {
-          backgroundColor: "#01A4F6", // Màu nền của header
-        },
-        headerTintColor: "white", // Màu chữ của header
+        }
       })}
     >
       <Tab.Screen name="ChatList" component={Chat} />
-      <Tab.Screen name="Contact" component={Contact} />
+      <Tab.Screen name="Contact" component={DirectoryComponents} />
       <Tab.Screen name="Explore" component={Explore} />
       <Tab.Screen name="News" component={News} />
       <Tab.Screen name="Info" component={FriendProfile} />
