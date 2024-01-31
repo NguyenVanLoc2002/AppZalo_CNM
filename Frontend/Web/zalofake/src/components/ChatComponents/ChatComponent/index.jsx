73bad4337ac1f5a799e98bd75a6aa606afeaa4d8
NguyenVanLoc2002 <nguyenvanloc6756@gmail.com>
2024-01-31 16:34:45 +0700

    commit UI chat continue

--- Frontend/Web/zalofake/src/components/ChatComponents/ChatComponent/index.jsx
+++ Frontend/Web/zalofake/src/components/ChatComponents/ChatComponent/index.jsx
@@ -6,7 +6,7 @@ function ChatComponents({ language }) {
   return (
     <>
       <div className="bg-gray-100 h-screen w-full flex">
-        <div className="h-screen w-full w-4/12 bg-white">
+        <div className="h-screen w-4/12 bg-white">
           <ListChatComponent language={language} />
         </div>
         <PeopleChatComponent language={language} />
