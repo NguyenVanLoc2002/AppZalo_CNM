df2ba6b55571741fbb7b01d342fd4708b02000af
NguyenVanLoc2002 <nguyenvanloc6756@gmail.com>
2024-01-31 16:34:45 +0700

    commit UI chat continue

--- Frontend/Web/zalofake/src/components/ChatComponents/PeopleChatComponent/index.jsx
+++ Frontend/Web/zalofake/src/components/ChatComponents/PeopleChatComponent/index.jsx
@@ -1,26 +1,43 @@
 import { useState } from "react";
 import { AiFillLike, AiOutlineUsergroupAdd } from "react-icons/ai";
-import { BiScreenshot, BiSmile } from "react-icons/bi";
-import { BsThreeDots } from "react-icons/bs";
-import { FaAddressCard, FaCaretDown, FaVideo } from "react-icons/fa";
+import { BiLockOpen, BiScreenshot, BiSmile } from "react-icons/bi";
+import { BsLayoutSidebarReverse, BsThreeDots } from "react-icons/bs";
+import { CiCircleQuestion } from "react-icons/ci";
+import {
+  FaAddressCard,
+  FaCaretDown,
+  FaRegEyeSlash,
+  FaUserFriends,
+  FaVideo,
+} from "react-icons/fa";
 import { FiCheckSquare } from "react-icons/fi";
 import { IoIosLink } from "react-icons/io";
-import { IoImageOutline } from "react-icons/io5";
+import {
+  IoImageOutline,
+  IoTrashOutline,
+  IoWarningOutline,
+} from "react-icons/io5";
 import { LuPencilLine, LuSticker } from "react-icons/lu";
 import { MdFormatColorText, MdPhone } from "react-icons/md";
 import {
+  PiAlarmThin,
   PiBellRingingThin,
   PiDesktopDuotone,
   PiMagnifyingGlass,
   PiTagSimpleLight,
 } from "react-icons/pi";
-import { RiBatteryChargeLine } from "react-icons/ri";
+import { RiAlarmLine, RiBatteryChargeLine } from "react-icons/ri";
 import { TfiAlarmClock } from "react-icons/tfi";
 import { TiPinOutline } from "react-icons/ti";
 
 function PeopleChatComponent({ language }) {
   const [content, setContent] = useState("");
   const chats = [];
+  const [isSidebarVisible, setSidebarVisible] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!isSidebarVisible);
+  };
 
   for (let i = 0; i < 10; i++) {
     chats.push(
@@ -51,7 +68,7 @@ function PeopleChatComponent({ language }) {
   }
   return (
     <>
-      <div className="h-screen w-full bg-white w-7/12 border-r">
+      <div className="h-screen w-full bg-white w-9/12 border-r relative z-0" >
         <div className="h-[70px] bg-white flex justify-between items-center border-b">
           <div className="flex items-center">
             <img src="zalo.svg" alt="avatar" />
@@ -72,8 +89,11 @@ function PeopleChatComponent({ language }) {
             <button className="hover:bg-gray-300 p-2 rounded">
               <FaVideo size={18} />
             </button>
-            <button className="bg-blue-100 p-2 rounded">
-              <PiDesktopDuotone size={18} fill="blue" />
+            <button
+              className="hover:bg-gray-300 p-2 rounded"
+              onClick={toggleSidebar}
+            >
+              <BsLayoutSidebarReverse size={18} />
             </button>
           </div>
         </div>
@@ -142,46 +162,167 @@ function PeopleChatComponent({ language }) {
           </div>
         </div>
       </div>
-      <div className="h-screen w-full bg-gray w-3/12">
-        <div className="h-[70px] bg-white flex justify-center items-center border-b">
-          <p className="text-lg font-medium">Thông tin hội thoại</p>
-        </div>
-        <div className="h-[calc(100%-70px)] items-center  overflow-y-auto">
-          <div className="h-[200px] bg-white items-center flex-col">
-            <div className="flex justify-center pt-2">
-              <img src="zalo.svg" alt="avatar" />
-            </div>
-            <div className="flex justify-center pt-2">
-              <p className="text-lg font-semibold">Le Nguyen Sinh</p>
-              <div className="flex items-center ml-2 rounded-full bg-gray-200 p-1 hover:bg-gray-400">
-                <button>
-                  <LuPencilLine size={20} />
-                </button>
+      {isSidebarVisible && (
+        <div className="fixed top-0 right-0 h-screen bg-gray w-4/12 z-10 bg-gray-300 border-l  drop-shadow-2xl">
+          <div className="h-[70px] bg-white flex justify-center items-center border-b">
+            <p className="flex text-lg font-medium">Thông tin hội thoại</p>
+            <button className="flex top-0 right-2 fixed text-lg font-medium" onClick={toggleSidebar}> 
+              x
+            </button>
+          </div>
+          <div className="h-[calc(100%-70px)] items-center  overflow-y-auto">
+            <div className="h-[200px] bg-white items-center flex-col">
+              <div className="flex justify-center pt-2">
+                <img src="zalo.svg" alt="avatar" />
+              </div>
+              <div className="flex justify-center pt-2">
+                <p className="text-lg font-semibold">Le Nguyen Sinh</p>
+                <div className="flex items-center ml-2 rounded-full bg-gray-200 p-1 hover:bg-gray-400">
+                  <button>
+                    <LuPencilLine size={20} />
+                  </button>
+                </div>
+              </div>
+              <div className="flex justify-around pt-2">
+                <div className="flex flex-col items-center">
+                  <button className="flex items-center  rounded-full bg-gray-200 p-1 hover:bg-gray-400">
+                    <PiBellRingingThin size={22} />
+                  </button>
+                  <p className="flex text-center">
+                    Tắt thông <br /> báo
+                  </p>
+                </div>
+                <div className="flex flex-col items-center ">
+                  <button className="flex items-center rounded-full bg-gray-200 p-1 hover:bg-gray-400">
+                    <TiPinOutline size={22} />
+                  </button>
+                  <p className="flex text-center">
+                    Ghim hội <br />
+                    thoại{" "}
+                  </p>
+                </div>
+                <div className="flex flex-col items-center">
+                  <button className="flex items-center  rounded-full bg-gray-200 p-1 hover:bg-gray-400">
+                    <AiOutlineUsergroupAdd size={22} />
+                  </button>
+                  <p className="flex text-center ">
+                    Tạo nhóm <br />
+                    trò truyện
+                  </p>
+                </div>
               </div>
             </div>
-            <div className="flex items-center justify-between pt-2">
-              <div className="flex-col items-center justify-center">
-                <button className="flex items-center ml-2 rounded-full bg-gray-200 p-1 hover:bg-gray-400">
-                  <PiBellRingingThin size={22} />
-                </button>
-                <p className="text-pretty flex items-center ">Tắt thông báo</p>
+
+            <div className=" flex flex-col justify-around mt-2  bg-white">
+              <button className="flex items-center p-2 hover:bg-gray-200">
+                <RiAlarmLine size={25} />
+                <p className="ml-2">Danh sách nhắc hẹn</p>
+              </button>
+
+              <button className="flex items-center  p-2 hover:bg-gray-200">
+                <FaUserFriends size={25} stroke="blue" />
+                <p className="ml-2">Nhóm chung</p>
+              </button>
+            </div>
+            <div className=" flex flex-col mt-2 p-2 bg-white">
+              <button className="flex items-center justify-between p-2">
+                <p className="font-medium text-lg ">Ảnh/Video</p>
+                <FaCaretDown size={20} />
+              </button>
+              <div>
+                <p className="text-center font-normal text-gray-400">
+                  Chưa có ảnh và video được chia sẻ
+                </p>
               </div>
-              <div className="flex-col items-center ">
-                <button className="flex items-center ml-2 rounded-full bg-gray-200 p-1 hover:bg-gray-400">
-                  <TiPinOutline size={22} />
-                </button>
-                <p className="text-pretty  ">Ghim hội thoại </p>
+            </div>
+            <div className=" flex flex-col mt-2 p-2 bg-white">
+              <button className="flex items-center justify-between p-2">
+                <p className="font-medium text-lg ">File</p>
+                <FaCaretDown size={20} />
+              </button>
+              <div>
+                <p className="text-center font-normal text-gray-400">
+                  Chưa có file được chia sẻ
+                </p>
               </div>
-              <div className="flex-col items-center">
-                <button className="flex items-center ml-2 rounded-full bg-gray-200 p-1 hover:bg-gray-400">
-                  <AiOutlineUsergroupAdd size={22} />
-                </button>
-                <p className="text-balance ">Tạo nhóm trò truyện</p>
+            </div>
+            <div className="flex flex-col mt-2 p-2 bg-white">
+              <button className="flex items-center justify-between p-2">
+                <p className="font-medium text-lg ">Link</p>
+                <FaCaretDown size={20} />
+              </button>
+              <div>
+                <p className="text-center font-normal text-gray-400">
+                  Chưa có link được chia sẻ
+                </p>
+              </div>
+            </div>
+            <div className=" flex flex-col mt-2 bg-white">
+              <button className="flex items-center justify-between mt-2 p-2 ml-2">
+                <p className="font-medium text-lg ">Thiết lập bảo mật</p>
+                <FaCaretDown size={20} />
+              </button>
+              <div className="flex flex-col">
+                <div className="flex items-center pt-2 pb-2 hover:bg-gray-200">
+                  <button className="ml-4">
+                    <BiLockOpen size={25} stroke="blue" />
+                  </button>
+                  <button className="flex flex-col ml-2">
+                    <p className="flex items-center">
+                      Mã hóa đầu cuối{" "}
+                      <CiCircleQuestion size={20} className="ml-2" />
+                    </p>
+                    <p className="font-normal text-sm text-gray-400">
+                      Chưa nâng cấp
+                    </p>
+                  </button>
+                </div>
+                <div className="flex items-center pt-2 pb-2 hover:bg-gray-200">
+                  <button className="ml-4">
+                    <PiAlarmThin size={25} stroke="blue" />
+                  </button>
+
+                  <button className="flex flex-col ml-2 ">
+                    <p className="flex items-center">
+                      Tin nhắn tự động xóa{" "}
+                      <CiCircleQuestion size={20} className="ml-2" />
+                    </p>
+                    <p className="font-normal text-sm text-gray-400">
+                      Không bao giờ
+                    </p>
+                  </button>
+                </div>
+                <div className="flex items-center justify-between pt-4 pb-4 hover:bg-gray-200">
+                  <button className="ml-4">
+                    <FaRegEyeSlash size={25} stroke="blue" />
+                  </button>
+
+                  <button className="flex items-center mr-auto ml-2 ">
+                    <p>Ẩn trò chuyện</p>
+                  </button>
+                  <input type="checkbox" className="toggle" />
+                </div>
+                <div className="flex items-center justify-between pt-4 pb-4 hover:bg-gray-200">
+                  <button className="ml-4">
+                    <IoWarningOutline size={25} />
+                  </button>
+                  <button className="flex items-center mr-auto ml-2 ">
+                    <p>Báo xấu</p>
+                  </button>
+                </div>
+                <div className="flex items-center justify-between pt-4 pb-4 hover:bg-gray-200">
+                  <button className="ml-4">
+                    <IoTrashOutline size={25} stroke="red" />
+                  </button>
+                  <button className="flex items-center mr-auto ml-2 text-red-600">
+                    <p>Xóa lịch sử trò chuyện</p>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
