b10a87cc76122d2101ab3d9b169a0b65810a7494
Bùi Trí Thức <20088361.thuc@student.iuh.edu.vn>
2024-02-07 14:20:59 +0700

    Update UI : Fix compatibility on Mobile

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,17 +1,13 @@
 //login user and create token and send it back
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
 
 const User = require("../models/User");
 const Media = require("../models/media");
-const mongoose = require("mongoose");
-const saltRounds = 10;
+const Session = require("../models/Session");
 
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
-};
+const saltRounds = 10;
 
 // hash password
 const hashPassword = async (password) => {
@@ -19,42 +15,81 @@ const hashPassword = async (password) => {
   return await bcrypt.hash(password, salt);
 };
 
-exports.registerUser = async (req, res) => {
-  const { name, password, phone } = req.body;
+const generateToken = (device_id, user_id) => {
+  return jwt.sign({ device_id, user_id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
+
+const createOrUpdateSession = async (user_id, device_id, app_type, token) => {
+  let session = await Session.findOne({ device_id, app_type });
+
+  if (session) {
+    session.is_logged_in = false;
+    await session.save();
+  }
+
+  // Tạo hoặc cập nhật phiên (session) mới
+  session = await Session.findOneAndUpdate(
+    { user_id, device_id, app_type },
+    { user_id, device_id, app_type, token, is_logged_in: true },
+    { upsert: true, new: true }
+  );
+
+  return session;
+};
+
+exports.loginUser = async (req, res) => {
+  const { phone, password, device_id, app_type } = req.body;
+
   try {
-    const user = await User.create({
-      name,
-      phone,
-      password: await hashPassword(password),
-    });
-    res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      phone: user.phone,
-      token: generateToken(user._id),
-    });
+    const user = await User.findOne({ phone });
+
+    if (user && (await user.matchPassword(password))) {
+      const token = generateToken(device_id, user._id);
+
+      const session = await createOrUpdateSession(
+        user._id,
+        device_id,
+        app_type,
+        token
+      );
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000, // 1 day
+      });
+      res.status(200).json({
+        user,
+        token,
+        session,
+      });
+    } else {
+      res.status(401).json({ message: "Invalid phone or password" });
+    }
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 };
 
-exports.loginUser = async (req, res) => {
-  const { phone, password } = req.body;
+exports.registerUser = async (req, res) => {
+  const { name, password, phone } = req.body;
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: "Password must be at least 6 characters" });
+  }
   try {
-    const user = await User.findOne({ phone });
-    if (user && (await user.matchPassword(password))) {
-      res.cookie("token", generateToken(user._id), {
-        httpOnly: true,
-        maxAge: 24 * 60 * 60 * 1000, // 30 days
-      });
-      res.status(200).json({
-        name: user.name,
-        phone: user.phone,
-        token: generateToken(user._id),
-      });
-    } else {
-      res.status(401).json({ message: "Invalid phone or password" });
-    }
+    const user = await User.create({
+      phone,
+      password: await hashPassword(password),
+      profile: { name },
+      createdAt: Date.now(),
+    });
+    res.status(201).json({
+      user,
+      token: generateToken(user._id),
+    });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -106,7 +141,6 @@ exports.uploadAvatar = async (req, res) => {
       contentType: file.mimetype,
       data: file.buffer,
     });
-
 
     const savedMedia = await newMedia.save();
 
