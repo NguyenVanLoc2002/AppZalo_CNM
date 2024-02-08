5407a98f19c9ce1c79099219e3265b5527e69ecc
Bùi Trí Thức <20088361.thuc@student.iuh.edu.vn>
2024-02-08 14:27:06 +0700

    Update UI : Fix compatibility on Mobile (V2)

--- Frontend/Mobile/zalofake/src/components/ChatComponents/friendProfile/FriendProfileSettings.js
+++ Frontend/Mobile/zalofake/src/components/ChatComponents/friendProfile/FriendProfileSettings.js
@@ -4,31 +4,31 @@ import { Ionicons } from '@expo/vector-icons';
 import { Switch } from 'react-native';
 const FriendProfileSettings = () => {
     const [isFavorite, setFavorite] = useState(false);
-const toggleFavorite = () => setFavorite(previousState => !previousState);
+const toggleFavorite = () => setFavorite(!isFavorite);
 
 const [isNotification, setNotification] = useState(true);
-const toggleNotification = () => setNotification(previousState => !previousState);
+const toggleNotification = () => setNotification(!isNotification);
 
 const [isBlockJournal, setBlockJournal] = useState(true);
-const toggleBlockJournal = () => setBlockJournal(previousState => !previousState);
+const toggleBlockJournal = () => setBlockJournal(!isBlockJournal);
 
 const [isHideJournal, setHideJournal] = useState(true);
-const toggleHideJournal = () => setHideJournal(previousState => !previousState);
+const toggleHideJournal = () => setHideJournal(!isHideJournal);
 
     return (
         <View style={{ flex: 1, backgroundColor: '#E5E9EB' }}>
             <View style={{ backgroundColor: 'white' }}>
                 <Pressable style={{ height: 50, paddingStart: 22, justifyContent: 'center' }}>
-                    <Text style={{ fontSize: 18 }}>Thông tin</Text>
+                    <Text style={{ fontSize: 16 }}>Thông tin</Text>
                 </Pressable>
                 <View style={{ height: 1, backgroundColor: 'gray', marginLeft: 20, marginEnd: 10, opacity: 0.2 }} />
                 <Pressable style={{ height: 50, paddingStart: 22, justifyContent: 'center' }}>
-                    <Text style={{ fontSize: 18 }}>Đổi tên gợi nhớ</Text>
+                    <Text style={{ fontSize: 16 }}>Đổi tên gợi nhớ</Text>
                 </Pressable>
                 <View style={{ height: 1, backgroundColor: 'gray', marginLeft: 20, marginEnd: 10, opacity: 0.2 }} />
                 <Pressable style={{ height: 50, paddingStart: 22, justifyContent: 'center' }}>
-                    <View style={{ flexDirection: 'row' }}>
-                        <Text style={{ fontSize: 18 }}>Đánh dấu bạn thân</Text>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Text style={{ fontSize: 16 }}>Đánh dấu bạn thân</Text>
                         <View style={{ flex: 1 }} />
 
                         <Switch
@@ -42,13 +42,13 @@ const toggleHideJournal = () => setHideJournal(previousState => !previousState);
                 </Pressable>
                 <View style={{ height: 1, backgroundColor: 'gray', marginLeft: 20, marginEnd: 10, opacity: 0.2 }} />
                 <Pressable style={{ height: 50, backgroundColor: 'white', paddingStart: 22, justifyContent: 'center' }}>
-                    <Text style={{ fontSize: 18 }}>Giới thiệu cho bạn bè</Text>
+                    <Text style={{ fontSize: 16 }}>Giới thiệu cho bạn bè</Text>
                 </Pressable>
             </View>
             <Pressable style={{ height: 80, backgroundColor: 'white', paddingStart: 22, justifyContent: 'center', marginTop: 10 }}>
-                <Text style={{ fontWeight: '600', fontSize: 18, color: '#0091FF' }}>Thông báo</Text>
-                <View style={{ flexDirection: 'row' }}>
-                    <Text style={{ fontSize: 18 }}>Nhận thông báo về hoạt động của người này</Text>
+                <Text style={{ fontWeight: '600', fontSize: 16, color: '#0091FF' }}>Thông báo</Text>
+                <View style={{ flexDirection: 'row', alignItems: "center" }}>
+                    <Text style={{ fontSize: 16 }}>Nhận thông báo về hoạt động của người này</Text>
                     <View style={{ flex: 1 }} />
 
                     <Switch
@@ -61,10 +61,10 @@ const toggleHideJournal = () => setHideJournal(previousState => !previousState);
                 </View>
             </Pressable>
             <View style={{ height: 130, backgroundColor: 'white', paddingStart: 22, marginTop: 10, justifyContent: 'center' }}>
-                <Text style={{ fontWeight: '600', fontSize: 18, color: '#0091FF' }}>Cài đặt riêng tư</Text>
+                <Text style={{ fontWeight: '600', fontSize: 16, color: '#0091FF' }}>Cài đặt riêng tư</Text>
                 <Pressable style={{ height: 50, justifyContent: 'center' }}>
-                    <View style={{ flexDirection: 'row' }}>
-                        <Text style={{ fontSize: 18 }}>Chặn xem nhật ký của tôi</Text>
+                    <View style={{ flexDirection: 'row', alignItems: "center" }}>
+                        <Text style={{ fontSize: 16 }}>Chặn xem nhật ký của tôi</Text>
                         <View style={{ flex: 1 }} />
                         <Switch
                             trackColor={{ false: '#767577', true: '#0091FF' }}
@@ -77,8 +77,8 @@ const toggleHideJournal = () => setHideJournal(previousState => !previousState);
                 </Pressable>
                 <View style={{ height: 1, backgroundColor: 'gray', paddingStart: 20, marginEnd: 10, opacity: 0.2 }} />
                 <Pressable style={{ height: 50, justifyContent: 'center' }}>
-                    <View style={{ flexDirection: 'row' }}>
-                        <Text style={{ fontSize: 18 }}>Ẩn nhật ký của người này</Text>
+                    <View style={{ flexDirection: 'row', alignItems:"center" }}>
+                        <Text style={{ fontSize: 16 }}>Ẩn nhật ký của người này</Text>
                         <View style={{ flex: 1 }} />
                         <Switch
                             trackColor={{ false: '#767577', true: '#0091FF' }}
@@ -93,11 +93,11 @@ const toggleHideJournal = () => setHideJournal(previousState => !previousState);
             <View style={{ height: 1, marginLeft: 20, marginRight: 10, opacity: 0.2 }} />
             <View style={{ flex: 1, backgroundColor: 'white', marginTop:10}}>
                 <Pressable style={{ height: 50, paddingStart: 22, justifyContent: 'center' }}>
-                    <Text style={{ fontSize: 18 }}>Báo xấu</Text>
+                    <Text style={{ fontSize: 16 }}>Báo xấu</Text>
                 </Pressable>
                 <View style={{ height: 1, backgroundColor: 'gray', marginLeft: 20, marginEnd: 10, opacity: 0.2 }} />
                 <Pressable style={{ height: 50, paddingStart: 22, justifyContent: 'center' }}>
-                    <Text style={{ fontSize: 18, color:'red' }}>Xóa bạn</Text>
+                    <Text style={{ fontSize: 16, color:'red' }}>Xóa bạn</Text>
                 </Pressable>
                 <View style={{ height: 1, backgroundColor: 'gray', marginLeft: 20, marginEnd: 10, opacity: 0.2 }} />
             </View>
