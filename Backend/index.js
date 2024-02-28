b681cd92da53d281281b007a4f272c2b96e20cfe
Bùi Trí Thức <20088361.thuc@student.iuh.edu.vn>
2024-02-29 01:00:55 +0700

    Update BE : Authentication v1

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const CookieParser = require("cookie-parser")
 require('dotenv').config()
 const { connectDB } = require('./configs/connectDBConfig.config')
 const userRoute = require('./routes/userRoute')
+const authRoute = require('./routes/authRouter')
 
 const app = express()
 app.use(express.json())
@@ -9,9 +11,12 @@ app.use(express.json())
 //connect DB
 connectDB()
 
+app.use(CookieParser())
+
 
 //routes
 app.use('/api/users', userRoute)
+app.use('/api/auth', authRoute)
 
 
 
