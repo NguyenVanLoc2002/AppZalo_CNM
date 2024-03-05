eb51bfc5161023c1b8d52085ccc91e9a8b1bf299
Bùi Trí Thức <20088361.thuc@student.iuh.edu.vn>
2024-03-06 00:38:37 +0700

    Update API : Fix Login API; Update Web App: Create login context and login hook

--- Backend/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { loginUser, refreshToken, registerUser, logoutUser } = require("../controllers/authController");
-
-router.post("/login", loginUser);
-router.post("/logout",logoutUser)
-router.post("/refreshToken", refreshToken);
-router.post("/register", registerUser);
-
-module.exports = router;
