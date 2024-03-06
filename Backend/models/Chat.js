04faa3c38114f6018f4bf4ca645f4a1261d29aea
NguyenVanLoc2002 <nguyenvanloc6756@gmail.com>
2024-03-06 20:19:36 +0700

    models v2

--- Backend/models/Chat.js
+++ Backend/models/Chat.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const chatSchema = new mongoose.Schema({
-  chatId: { type: String, required: true },
   senderId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
