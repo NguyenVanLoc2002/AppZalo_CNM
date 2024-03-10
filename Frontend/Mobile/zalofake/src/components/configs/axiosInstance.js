5eaa1bf43ba4dcf4f41754ef97e890ff79e88699
BaoTruc <tranbaotruc2002bc@gmail.com>
2024-03-10 20:31:13 +0700

    Update Mobile: config and fix register

---
+++ Frontend/Mobile/zalofake/src/components/configs/axiosInstance.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import config from './config';
+
+const axiosInstance = axios.create({
+  baseURL: config.baseURL,
+  timeout: 5000 
+});
+
+export default axiosInstance;
+
+
