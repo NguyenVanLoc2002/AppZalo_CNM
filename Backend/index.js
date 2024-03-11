e45807a831f808786ff02185ac91edd3309b4851
NguyenVanLoc2002 <nguyenvanloc6756@gmail.com>
2024-03-12 00:32:11 +0700

    chat single v1

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,34 @@
-const express = require('express')
-const CookieParser = require("cookie-parser")
-const cors = require('cors')
-const dotenv = require('dotenv')
-const { connectDB } = require('./configs/connectDBConfig.config')
-const userRoute = require('./routes/user.routes')
-const authRoute = require('./routes/auth.routes')
-const {app, server} = require('./socket/socket.io')    
+const express = require("express");
+const CookieParser = require("cookie-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { connectDB } = require("./configs/connectDBConfig.config");
+const userRoute = require("./routes/user.routes");
+const authRoute = require("./routes/auth.routes");
+const chatRouter = require("./routes/chat.router");
+const { app, server } = require("./socket/socket.io");
+const authMiddleware = require("./middlewares/authMiddleware");
 
 
+const bodyParser = require('body-parser');
 
-dotenv.config()
-app.use(cors())
-app.use(express.json())
-app.use(CookieParser())
+// Sử dụng body-parser middleware để xử lý dữ liệu form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use(CookieParser());
+
 
 //connect DB
-connectDB()
+connectDB();
 
 //routes
-app.use('/api/users', userRoute)
-app.use('/api/auth', authRoute)
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/chats",authMiddleware.protect,chatRouter);
 
-
-
-server.listen(process.env.PORT,()=>{console.log(`Server listiening on ${process.env.PORT}`);})
+server.listen(process.env.PORT, () => {
+  console.log(`Server listiening on ${process.env.PORT}`);
+});
