a23fd428f1e6ae96d8733f98be092cde3fd8cc56
NguyenVanLoc2002 <nguyenvanloc6756@gmail.com>
2024-03-23 22:46:10 +0700

    chatController v2

--- Backend/routes/chat.router.js
+++ Backend/routes/chat.router.js
@@ -4,19 +4,25 @@ const {
   getHistoryMessage,
   sendMessage,
 } = require("../controllers/chat.controller");
-const multerUpload= require("../middlewares/multerMiddleware");
-const {formatBodyData} = require('../middlewares/bodyDataFormat');
+const {
+  multerUploadImage,
+  multerUploadVideo,
+} = require("../middlewares/multerMiddleware");
+const { formatBodyData } = require("../middlewares/bodyDataFormat");
 
-// Lấy danh sách tin nhắn cá nhân với một người dùng cụ thể với userId là người nhận 
+
+// Lấy danh sách tin nhắn cá nhân với một người dùng cụ thể với userId là người nhận
 router.get("/:userId", getHistoryMessage);
 
-// Gửi tin nhắn mới cho một người dùng cụ thể
-router.post("/:userId/send",multerUpload.array('data'),async(req, res)=>{console.log(req.files);}, formatBodyData,sendMessage);
-// ,async(req, res)=>{console.log(req.files); console.log(req.body);}
+// Gửi tin nhắn mới cho một người dùng cụ thể bao gồm text và image
+router.post("/:userId/sendMessage",  multerUploadImage.array("data"), formatBodyData, sendMessage);
+
+// Gửi video mới cho một người dùng cụ thể 
+router.post("/:userId/sendVideo", multerUploadVideo.array("data"), formatBodyData, sendMessage);
 
 //Xử lý lỗi từ Multer
 router.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).json({ error: 'Something went wrong'});
+  res.status(500).json({ error: "Something went wrong" });
 });
-module.exports= router;
+module.exports = router;
