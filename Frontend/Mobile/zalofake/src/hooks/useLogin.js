36cbee95feaa7a718ced2ef1d3d3280f905d9ccb
yennhii134 <yennhi1342@email.com>
2024-03-24 01:48:10 +0700

    logout v1

---
+++ Frontend/Mobile/zalofake/src/hooks/useLogin.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { useAuthContext } from "../contexts/AuthContext";
+import Toast from "react-native-toast-message";
+import * as Device from "expo-device";
+
+import axiosInstance from "../components/configs/axiosInstance";
+
+const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+
+  const { setAuthUser, setAccessToken, setRefreshToken } = useAuthContext();
+
+  const login = async (phone, password) => {
+    setLoading(true);
+    try {
+      const device_id = Device.osBuildId;
+      const response = await axiosInstance.post("/auth/login", {
+        phone,
+        password,
+        device_id,
+      });
+
+      const data = response.data;
+      if (response.status === 200) {
+        setAuthUser(data.user);
+        setAccessToken(data.accessToken);
+        setRefreshToken(data.refreshToken);
+      } else {
+        showMesg("Error during login", "error");
+      }
+    } catch (error) {
+      if (error.request) {
+        showMesg("Error server, please try again !", "error");
+        throw error;
+      } else {
+        showMesg("Error during login", "error");
+        throw error;
+      }
+    }
+    setLoading(false);
+  };
+
+  return { login, loading };
+};
+
+const showMesg = (mesg, type) => {
+  Toast.show({
+    type: `${type}`,
+    text1: `${mesg}`,
+    text2: `${mesg}`,
+  });
+};
+
+export default useLogin;
