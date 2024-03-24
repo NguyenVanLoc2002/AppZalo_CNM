c443526825010d5b67862bbfd64684d227209833
Bùi Trí Thức <20088361.thuc@student.iuh.edu.vn>
2024-03-24 21:58:48 +0700

    Update And Fix Bug: -Fix Bug: login, logout, store data after login and after logout - Fix UI: Fix throw error message - Update feature : login, logout on both platform - Update feature : show user info on web

--- Frontend/Mobile/zalofake/src/contexts/AuthContext.js
+++ Frontend/Mobile/zalofake/src/contexts/AuthContext.js
@@ -7,7 +7,7 @@ export const useAuthContext = () => {
   return useContext(AuthContext);
 };
 
-AsyncStorage.clear();
+// AsyncStorage.clear();
 export const AuthContextProvider = ({ children }) => {
   const [authUser, setAuthUser] = useState(null);
   const [accessToken, setAccessToken] = useState(null);
