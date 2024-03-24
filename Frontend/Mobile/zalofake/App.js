29b7eff6e84268d3290d8ee2fcf75e1673dc4577
Bùi Trí Thức <20088361.thuc@student.iuh.edu.vn>
2024-03-24 21:58:48 +0700

    Update And Fix Bug: -Fix Bug: login, logout, store data after login and after logout - Fix UI: Fix throw error message - Update feature : login, logout on both platform - Update feature : show user info on web

--- Frontend/Mobile/zalofake/App.js
+++ Frontend/Mobile/zalofake/App.js
@@ -1,6 +1,6 @@
 import Toast from "react-native-toast-message";
 import { NavigationContainer } from "@react-navigation/native";
-import Main from "./RootComponent";
+import Main from "./src/pages/RootComponent";
 import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 export default function App() {
