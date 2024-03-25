c6ed8fb51d1da4535556a446e32653873a2cdbf0
Bùi Trí Thức <20088361.thuc@student.iuh.edu.vn>
2024-03-26 01:10:19 +0700

    Add feature : user update avatar, user update background, user update profile

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -5,22 +5,30 @@ const {
   getUserByPhone,
   sendOTP,
   uploadAvatar,
+  uploadBackground,
+  updateUser,
 } = require("../controllers/user.controller");
 const { protect, isRootUser } = require("../middlewares/authMiddleware");
-const {multerUploadImage}= require("../middlewares/multerMiddleware");
+const { multerUploadImage } = require("../middlewares/multerMiddleware");
 
 //get methods
 router.get("/get/:phone", protect, isRootUser, getUserByPhone);
-router.get("/get/:phone", protect, isRootUser, getUserByPhone);
+router.get("/get/:uid", protect, getUserByPhone);
 
 //post methods
-router.post("/otp/send", sendOTP);
+router.post("/update-profile", protect, updateUser);
+
 router.post(
   "/upload-avatar",
   protect,
   multerUploadImage.single("avatar"),
   uploadAvatar
 );
-
+router.post(
+  "/upload-background",
+  protect,
+  multerUploadImage.single("background"),
+  uploadBackground
+);
 
 module.exports = router;
