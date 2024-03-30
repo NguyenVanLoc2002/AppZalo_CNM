a90d834debbe550cf4d8574d09702e598210256e
zzQuocHao0201zz <20092211.hao@student.iuh.edu.vn>
2024-03-30 11:01:21 +0700

    Implement user info display on login

--- Frontend/Web/zalofake/src/layouts/Sidebar/index.jsx
+++ Frontend/Web/zalofake/src/layouts/Sidebar/index.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import { BsChatText, BsChatTextFill } from "react-icons/bs";
 import { RiContactsBookLine, RiContactsBookFill } from "react-icons/ri";
 import { FaCheckSquare } from "react-icons/fa";
@@ -8,114 +7,97 @@ import { IoSettingsOutline, IoSettingsSharp } from "react-icons/io5";
 import { CiCloudOn } from "react-icons/ci";
 
 function Sidebar({ changeTab, changeMenu }) {
-  const [tabSelected, setTabSelected] = useState();
-  const [settingClick, setsettingClick] = useState(false);
-  const user = localStorage.getItem("authUser");
-  const avtUrl = user ? JSON.parse(user).profile.avatar.url : "public/zalo.svg";
+  const [tabSelected, setTabSelected] = useState(1);
+  const [settingClick, setSettingClick] = useState(false);
+  const [avtUrl, setAvtUrl] = useState("public/zalo.svg"); // Set giá trị mặc định
+
+  useEffect(() => {
+    const user = localStorage.getItem("authUser");
+    if (user) {
+      const userData = JSON.parse(user);
+      if (
+        userData.profile &&
+        userData.profile.avatar &&
+        userData.profile.avatar.url
+      ) {
+        setAvtUrl(userData.profile.avatar.url);
+      }
+    }
+  }, []); // Sử dụng useEffect để lấy dữ liệu từ localStorage khi component được render lần đầu
 
   return (
-    <>
-      <div className="bg-[#0091ff] h-full flex flex-col items-center justify-between">
+    <div className="bg-[#0091ff] h-full flex flex-col items-center justify-between">
+      <div>
+        <div
+          className="h-20 w-[75px] flex justify-center items-center mt-5"
+          onClick={() => changeMenu(1)}
+        >
+          <img src={avtUrl} alt="avatar" className="w-14 h-14 rounded-full" />
+        </div>
         <div>
-          <div
-            className="h-20 w-[75px] flex justify-center items-center mt-5"
-            onClick={() => {
-              changeMenu(1);
-            }}
-          >
-            <img src={avtUrl} alt="avatar" className="w-14 h-14 rounded-full" />
-          </div>
-          <div>
+          {[1, 2, 3].map((tab) => (
             <div
-              className={[
-                " h-20 w-[75px] flex justify-center items-center hover:bg-[#006edc] ",
-                tabSelected == 1 ? " bg-[#006edc] " : " ",
-              ]}
+              key={tab}
+              className={`h-20 w-[75px] flex justify-center items-center hover:bg-[#006edc] ${
+                tabSelected === tab ? "bg-[#006edc]" : ""
+              }`}
               onClick={() => {
-                setTabSelected(1);
-                changeTab(1);
-                setsettingClick(false);
+                setTabSelected(tab);
+                changeTab(tab);
+                setSettingClick(false);
                 changeMenu();
               }}
             >
-              {tabSelected == 1 ? (
-                <BsChatTextFill size={28} color="#ffffff" />
-              ) : (
+              {tabSelected === tab ? (
+                tab === 1 ? (
+                  <BsChatTextFill size={28} color="#ffffff" />
+                ) : tab === 2 ? (
+                  <RiContactsBookFill size={28} color="#ffffff" />
+                ) : (
+                  <FaCheckSquare size={28} color="#ffffff" />
+                )
+              ) : tab === 1 ? (
                 <BsChatText size={28} color="#ffffff" />
-              )}
-            </div>
-            <div
-              className={[
-                " h-20 w-[75px] flex justify-center items-center hover:bg-[#006edc] ",
-                tabSelected == 2 ? " bg-[#006edc] " : " ",
-              ]}
-              onClick={() => {
-                setTabSelected(2);
-                changeTab(2);
-                setsettingClick(false);
-                changeMenu();
-              }}
-            >
-              {tabSelected == 2 ? (
-                <RiContactsBookFill size={28} color="#ffffff" />
-              ) : (
+              ) : tab === 2 ? (
                 <RiContactsBookLine size={28} color="#ffffff" />
-              )}
-            </div>
-            <div
-              className={[
-                " h-20 w-[75px] flex justify-center items-center hover:bg-[#006edc] ",
-                tabSelected == 3 ? " bg-[#006edc] " : " ",
-              ]}
-              onClick={() => {
-                setTabSelected(3);
-                changeTab(3);
-                setsettingClick(false);
-                changeMenu();
-              }}
-            >
-              {tabSelected == 3 ? (
-                <FaCheckSquare size={28} color="#ffffff" />
               ) : (
                 <FaRegSquareCheck size={28} color="#ffffff" />
               )}
             </div>
-          </div>
-        </div>
-        <div>
-          <div
-            className={[
-              "h-20 w-[75px] flex justify-center items-center hover:bg-[#006edc] active:bg-[#006edc]",
-              tabSelected == 4 ? " bg-[#006edc] " : " ",
-            ]}
-            onClick={() => {
-              setTabSelected(4);
-              changeTab(4);
-              setsettingClick(false);
-              changeMenu();
-            }}
-          >
-            <CiCloudOn size={28} color="#ffffff" />
-          </div>
-          <div
-            className={[
-              " h-20 w-[75px] flex justify-center items-center hover:bg-[#006edc] ",
-              settingClick ? " bg-[#006edc] " : " ",
-            ]}
-            onClick={() => {
-              setsettingClick(!settingClick);
-              changeMenu(2);
-            }}
-          >
-            {settingClick ? (
-              <IoSettingsSharp size={28} color="#ffffff" />
-            ) : (
-              <IoSettingsOutline size={28} color="#ffffff" />
-            )}
-          </div>
+          ))}
         </div>
       </div>
-    </>
+      <div>
+        <div
+          className={`h-20 w-[75px] flex justify-center items-center hover:bg-[#006edc] active:bg-[#006edc] ${
+            tabSelected === 4 ? "bg-[#006edc]" : ""
+          }`}
+          onClick={() => {
+            setTabSelected(4);
+            changeTab(4);
+            setSettingClick(false);
+            changeMenu();
+          }}
+        >
+          <CiCloudOn size={28} color="#ffffff" />
+        </div>
+        <div
+          className={`h-20 w-[75px] flex justify-center items-center hover:bg-[#006edc] ${
+            settingClick ? "bg-[#006edc]" : ""
+          }`}
+          onClick={() => {
+            setSettingClick(!settingClick);
+            changeMenu(2);
+          }}
+        >
+          {settingClick ? (
+            <IoSettingsSharp size={28} color="#ffffff" />
+          ) : (
+            <IoSettingsOutline size={28} color="#ffffff" />
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
 
