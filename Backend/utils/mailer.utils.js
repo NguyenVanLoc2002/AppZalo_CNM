da99a315eae27c921d0504d01afb59e039667eca
Bùi Trí Thức <20088361.thuc@student.iuh.edu.vn>
2024-03-30 16:26:52 +0700

    Update feature : - Auth Feature : update register paramaster, Send OTP to email

---
+++ Backend/utils/mailer.utils.js
@@ -0,0 +1,20 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const transporter = require("../configs/Mailer.config");
+
+const sendMail = async (to, subject, html) => {
+  try {
+    const rs = await transporter.sendMail({
+      from: process.env.NODE_MAILER_USER,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Message sent: %s", rs.messageId);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = sendMail;
