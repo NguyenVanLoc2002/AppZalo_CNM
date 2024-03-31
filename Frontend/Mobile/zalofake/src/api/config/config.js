16704ac8aa3ce7559c5d4086ca9fac33d6a2ead5
yennhii134 <yennhi13402@gmail.com>
2024-04-01 01:52:17 +0700

    push to pull, thieu file

--- Frontend/Mobile/zalofake/src/api/config/config.js
+++ Frontend/Mobile/zalofake/src/api/config/config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-  baseURL: "http://192.168.1.76:3000/api", // Thay đổi địa chỉ IP của máy chủ tại đây
+  baseURL: "http://192.168.20.103:3000/api", // Thay đổi địa chỉ IP của máy chủ tại đây
 };
 
 export default config;
