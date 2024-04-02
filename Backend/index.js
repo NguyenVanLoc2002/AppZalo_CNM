b0984820d6a95c8f66c218390da4ccf8bf9c7d3e
Bùi Trí Thức <20088361.thuc@student.iuh.edu.vn>
2024-04-02 19:27:49 +0700

    Update : - BE : Fix socket io connection, fix user update and token - FE : (On web platform) : Update load friend list and select to chat

--- Backend/index.js
+++ Backend/index.js
@@ -3,17 +3,19 @@ const CookieParser = require("cookie-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { connectDB } = require("./configs/connectDBConfig.config");
-const { app, server } = require("./socket/socket.io");
 const userRoute = require("./routes/user.routes");
 const authRoute = require("./routes/auth.routes");
+const { app, server } = require("./socket/socket.io");
+const authMiddleware = require("./middlewares/authMiddleware");
+const chatRouter = require("./routes/chat.routes");
+
+const bodyParser = require("body-parser");
+
+// Sử dụng body-parser middleware để xử lý dữ liệu form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 dotenv.config();
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(CookieParser());
 
@@ -23,10 +25,10 @@ connectDB();
 //routes
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/chats", authMiddleware.protect, chatRouter);
 
 server.listen(process.env.PORT, () => {
-  console.log(`Server listiening on ${process.env.PORT}`);
-  // get ip address of the server
+
   const os = require("os");
   const ifaces = os.networkInterfaces();
   let ipAddress = "";
