b133ba4d883e8d366a54af81e1de02a7405995f9
zzQuocHao0201zz <20092211.hao@student.iuh.edu.vn>
2024-04-02 16:16:49 +0700

    update brg web v1

--- Frontend/Web/zalofake/src/hooks/useUpdate.js
+++ Frontend/Web/zalofake/src/hooks/useUpdate.js
@@ -33,7 +33,7 @@ const useUpdate = () => {
       if (status === 200) {
         const { avatar } = data;
         if (avatar) {
-          toast.success("Avatar updated successfully");
+          // toast.success("Avatar updated successfully");
         } else {
           throw new Error("Failed to update avatar");
         }
@@ -50,14 +50,54 @@ const useUpdate = () => {
     }
   };
 
-  const updateProfile = async (userData) => {
-    console.log(userData);
+  const updateBackground = async (file, authToken) => {
     setLoading(true);
     try {
-      // if (!authToken) {
-      //   throw new Error("Authentication token not found");
-      // }
+      if (!authToken) {
+        throw new Error("Authentication token not found");
+      }
 
+      const formData = new FormData();
+      formData.append("background", file);
+
+      const response = await axiosInstance.post(
+        "/users/upload-background",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      const { data, status } = response;
+
+      if (status === 200) {
+        const { background } = data;
+        if (background) {
+          // Cập nhật ảnh bìa thành công
+          // Thực hiện các thao tác cần thiết sau khi cập nhật ảnh bìa
+          // Ví dụ: hiển thị thông báo, cập nhật state, v.v.
+        } else {
+          throw new Error("Failed to update background");
+        }
+      } else {
+        throw new Error(data.message || "Failed to update background");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(
+        error.message || "Failed to update background! Please try again."
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const updateProfile = async (userData) => {
+    setLoading(true);
+    try {
       const response = await axiosInstance.post(
         "/users/update-profile",
         userData
@@ -87,7 +127,7 @@ const useUpdate = () => {
     }
   };
 
-  return { updateAvatar, updateProfile, loading };
+  return { updateAvatar, updateBackground, updateProfile, loading };
 };
 
 export default useUpdate;
