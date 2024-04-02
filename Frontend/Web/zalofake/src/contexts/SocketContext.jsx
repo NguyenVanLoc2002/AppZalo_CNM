db5f1653a2d1568d9d4a9a89f3f9cc1124712b41
Bùi Trí Thức <20088361.thuc@student.iuh.edu.vn>
2024-04-02 19:27:49 +0700

    Update : - BE : Fix socket io connection, fix user update and token - FE : (On web platform) : Update load friend list and select to chat

---
+++ Frontend/Web/zalofake/src/contexts/SocketContext.jsx
@@ -0,0 +1,63 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import io from "socket.io-client";
+import { useAuthContext } from "./AuthContext";
+import toast from "react-hot-toast";
+
+const SocketContext = createContext();
+export const useSocketContext = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketContextProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const [onlineFriends, setOnlineFriends] = useState([]);
+  const [isDisplayToast, setIsDisplayToast] = useState(false);
+  const { refreshToken, setAuthUser, setAccessToken, setRefreshToken } =
+    useAuthContext();
+
+  const connectSocket = (token) => {
+    const newSocket = io("http://localhost:3000", {
+      query: {
+        token: token,
+      },
+    });
+    setSocket(newSocket);
+  };
+
+  useEffect(() => {
+    if (refreshToken) {
+      connectSocket(refreshToken);
+    } else {
+      setSocket(null);
+      socket?.disconnect();
+    }
+    return () => {
+      if (socket) {
+        socket?.disconnect();
+      }
+    };
+  }, [refreshToken]);
+
+  if (socket) {
+    socket.on("force_logout", () => {
+      setAuthUser(null);
+      setRefreshToken(null);
+      setAccessToken(null);
+      if (!isDisplayToast) {
+        toast.error("Your account has been logged out from another device");
+        socket.disconnect();
+        setIsDisplayToast(true);
+      }
+    });
+    socket.on("online_friends", (onlineFriends) => {
+      setOnlineFriends(onlineFriends);
+    });
+    
+  }
+
+  return (
+    <SocketContext.Provider value={{ socket, onlineFriends }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
