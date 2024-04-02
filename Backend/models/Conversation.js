96789a20726b6c067774d90b75b63e645fd39ab8
NguyenVanLoc2002 <nguyenvanloc6756@gmail.com>
2024-04-02 20:52:05 +0700

    up 4

--- Backend/models/Conversation.js
+++ Backend/models/Conversation.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 
 const ConversationSchema = new mongoose.Schema({
-    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
+    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
+    messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'chats' }],
     createdAt: { type: Date, default: Date.now },
     tag: { type: String, optional: true },
   });
