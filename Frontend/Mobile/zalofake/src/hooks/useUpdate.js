bfacf3e287458206f9ec94be08b073991feeda5c
zzQuocHao0201zz <20092211.hao@student.iuh.edu.vn>
2024-04-03 22:57:34 +0700

    commit for clean working tree

--- Frontend/Mobile/zalofake/src/hooks/useUpdate.js
+++ Frontend/Mobile/zalofake/src/hooks/useUpdate.js
@@ -106,7 +106,6 @@ const useUpdate = () => {
         "/users/update-profile",
         userData
       );
-      console.log(response);
 
       const { data, status } = response;
       console.log(data);
