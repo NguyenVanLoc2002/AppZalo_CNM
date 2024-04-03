7e7941dfd8dd4ba4201f6eec64ff2a3778c843b1
zzQuocHao0201zz <20092211.hao@student.iuh.edu.vn>
2024-04-03 14:55:55 +0700

    update mobile v1

--- Frontend/Mobile/zalofake/src/api/config.js
+++ Frontend/Mobile/zalofake/src/api/config.js
@@ -1,14 +1,6 @@
 // config.js
 const config = {
-<<<<<<< HEAD
-<<<<<<< HEAD
   baseURL: "http://192.168.2.8:3000/api", // Thay đổi địa chỉ IP của máy chủ tại đây
-=======
-  baseURL: "http://192.168.1.5:3000/api", // Thay đổi địa chỉ IP của máy chủ tại đây
->>>>>>> 6dacaca926d2a7327893bb50b693a7870b430608
-=======
-  baseURL: "http://192.168.20.103:3000/api", // Thay đổi địa chỉ IP của máy chủ tại đây
->>>>>>> fd8078664ae63edd525eecfa08f84476c46796a1
 };
 
 export default config;
