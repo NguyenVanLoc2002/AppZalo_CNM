c3d3bd009cd1f49bf2fe8050c0439e4bc1c41573
yennhii134 <yennhi13402@gmail.com>
2024-04-04 15:25:38 +0700

    Update Moblie: update handle change password

--- Frontend/Mobile/zalofake/src/api/axiosInstance.js
+++ Frontend/Mobile/zalofake/src/api/axiosInstance.js
@@ -1,21 +1,27 @@
 import axios from "axios";
 import config from "./config";
-import toast from "react-native-toast-message";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import Toast from "react-native-toast-message";
 
 const axiosInstance = axios.create({
-  baseURL: config.baseURL
+  baseURL: config.baseURL,
 });
-
-
-
+// AsyncStorage.clear()
 axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = JSON.parse(AsyncStorage.getItem("accessToken"));
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+  async (config) => {
+    try {
+      config.headers["User-Agent"] = "Mobile";
+      if (!config.url.includes("/auth/login")) {
+        const token = JSON.parse(await AsyncStorage.getItem("accessToken"));
+        // console.log(token);
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+      }
+      return config;
+    } catch (error) {
+      return Promise.reject(error);
     }
-    return config;
   },
   (error) => {
     return Promise.reject(error);
@@ -27,36 +33,51 @@ axiosInstance.interceptors.response.use(
     return response;
   },
   async (error) => {
-    console.log("hihi");
     const originalRequest = error.config;
-    const reqURL = originalRequest && originalRequest.url;
-
-    if (error.response.status === 401 && !reqURL.includes("/auth/login")) {
+    if (
+      error.response.status === 401 &&
+      !originalRequest.url.includes("auth/login")
+    ) {
       try {
-        const refreshToken = JSON.parse(AsyncStorage.getItem("refreshToken"));
+        const refreshToken = JSON.parse(
+          await AsyncStorage.getItem("refreshToken")
+        );
         if (!refreshToken) {
-          toast.error("Your session has expired. Please login again.");
+          showErrorToast("Your session has expired. Please login again.");
           throw new Error("No refresh token available.");
         }
 
-        const refreshedTokenResponse = await axiosInstance.post("/auth/refreshToken", {
-          refreshToken: refreshToken
-        });
+        const refreshedTokenResponse = await axiosInstance.post(
+          "/auth/refreshToken",
+          {
+            refreshToken: refreshToken,
+          }
+        );
         const newAccessToken = refreshedTokenResponse.data.newAccessToken;
         AsyncStorage.setItem("accessToken", JSON.stringify(newAccessToken));
 
         originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
         return axiosInstance(originalRequest);
-
       } catch (refreshError) {
         console.error("Refresh token failed:", refreshError);
-        toast.error("Your session has expired. Please login again.");
-        AsyncStorage.clear();
+        showErrorToast("Your session has expired. Please login again.");
+        await AsyncStorage.clear();
         return Promise.reject(refreshError);
       }
     }
     return Promise.reject(error);
   }
 );
+
+const showErrorToast = (message) => {
+  Toast.show({
+    type: "error",
+    text1: "Error",
+    text2: message,
+    position: "bottom",
+    visibilityTime: 4000,
+    autoHide: true,
+  });
+};
 
 export default axiosInstance;
