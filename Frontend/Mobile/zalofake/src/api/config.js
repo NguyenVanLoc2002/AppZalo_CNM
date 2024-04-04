b4a85971b57974ca44ee72e1fc9ad62cb1aef5ae
yennhii134 <yennhi13402@gmail.com>
2024-04-05 03:53:09 +0700

    Update Moblie: add UI ForgotPassword in ChangePassword

--- Frontend/Mobile/zalofake/src/api/config.js
+++ Frontend/Mobile/zalofake/src/api/config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-  baseURL: "http://192.168.3.16:3000/api", // Thay đổi địa chỉ IP của máy chủ tại đây
+  baseURL: "http://192.168.20.106:3000/api", // Thay đổi địa chỉ IP của máy chủ tại đây
 };
 
 export default config;
