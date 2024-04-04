05f8251dd61fba54f89f6a249e027bb19459d32d
yennhii134 <yennhi13402@gmail.com>
2024-04-04 15:25:38 +0700

    Update Moblie: update handle change password

--- Frontend/Mobile/zalofake/src/components/InfoComponents/ChangePassword.js
+++ Frontend/Mobile/zalofake/src/components/InfoComponents/ChangePassword.js
@@ -1,15 +1,17 @@
-import { View, Text, Pressable, TouchableOpacity } from "react-native";
+import { View, Text, Pressable, TouchableOpacity, ScrollView, Modal } from "react-native";
 import React, { useEffect, useState } from "react";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import FontAwesomeIcons from "react-native-vector-icons/FontAwesome5";
 import { TextInput } from "react-native-paper";
-import { useAuthContext } from "../../contexts/AuthContext";
 import useChangePw from "../../hooks/useChangePw";
 import Toast from "react-native-toast-message";
+import useLogout from "../../hooks/useLogout";
 
 const ChangePassword = ({ navigation, route }) => {
-    const { authUser } = useAuthContext();
     const { showToastSuccess, showToastError, changePassword } = useChangePw();
+    const logout = useLogout();
+    const [modalSuccess, setModalSuccess] = useState(false);
+
 
     useEffect(() => {
         navigation.setOptions({
@@ -65,78 +67,122 @@ const ChangePassword = ({ navigation, route }) => {
         else {
             const rs = await changePassword(oldPassword, newPassword);
             if (rs) {
-                showToastSuccess("Success")
+                setModalSuccess(!modalSuccess)
             }
-
+        }
+    }
+    const handleLogout = async () => {
+        setModalSuccess(!modalSuccess)
+        try {
+            await logout();
+        } catch (error) {
+            console.log(error);
+            showToastError("Lỗi ! Vui lòng thử lại sau");
         }
     }
 
 
-    return (
-        <View style={{ backgroundColor: 'white', height: '100%', alignItems: 'center' }}>
-            <View style={styles.toastContainer}>
-                <Toast />
-            </View>
-            <View style={{ width: '90%' }}>
-                <View style={[{ height: 75 }, styles.styleCenter]}>
-                    <Text style={{ fontSize: 16, fontWeight: '600', textAlign: 'center' }}>Mật khẩu phải gồm 6 kí tự bao gồm chữ, số hoặc ký tự đặc biệt.</Text>
-                </View>
-                <View style={styles.styleViewBlue}>
-                    <Text style={[styles.styleText, styles.styleColorBlue]}>Mật khẩu hiện tại
-                    </Text>
-                    {/* <Text style={[styles.styleText]}>Hiện</Text> */}
-                    <TouchableOpacity onPress={togglePasswordVisibility}>
-                        <View>
-                            <Text style={[styles.styleText]}>{buttonText}</Text>
 
+    return (
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+            <ScrollView>
+                <View style={{ alignItems: 'center' }}>
+                    <View style={styles.toastContainer}>
+                        <Toast />
+                    </View>
+                    <View style={{ width: '90%' }}>
+                        <View style={[{ height: 75 }, styles.styleCenter]}>
+                            <Text style={{ fontSize: 16, fontWeight: '600', textAlign: 'center' }}>Mật khẩu phải gồm 6 kí tự bao gồm chữ, số hoặc ký tự đặc biệt.</Text>
                         </View>
-                    </TouchableOpacity>
+                        <View style={styles.styleViewBlue}>
+                            <Text style={[styles.styleText, styles.styleColorBlue]}>Mật khẩu hiện tại
+                            </Text>
+                            {/* <Text style={[styles.styleText]}>Hiện</Text> */}
+                            <TouchableOpacity onPress={togglePasswordVisibility}>
+                                <View>
+                                    <Text style={[styles.styleText]}>{buttonText}</Text>
+
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.styleViewGray}>
+                            <TextInput style={[styles.styleText, styles.styleColorGray, { width: '90%' }]}
+                                value={oldPassword}
+                                onChangeText={setOldPassword}
+                                placeholder="Nhập mật khẩu hiện tại"
+                                secureTextEntry={!showPassword}
+                            ></TextInput>
+                            {oldPassword ? (
+                                <Pressable style={styles.styleIcon} onPress={() => setOldPassword(null)}>
+                                    <Ionicons name="close" size={22} color="gray" />
+                                </Pressable>) : null}
+                        </View>
+                        <View style={styles.styleViewBlue}>
+                            <Text style={[styles.styleText, styles.styleColorBlue]}>Mật khẩu mới</Text>
+                        </View>
+                        <View style={styles.styleViewGray}>
+                            <TextInput style={[styles.styleText, styles.styleColorGray, { width: '90%' }]}
+                                value={newPassword}
+                                onChangeText={setNewPassword}
+                                placeholder="Nhập mật khẩu mới"
+                                secureTextEntry={!showPassword}
+                            ></TextInput>
+                            {newPassword ? (
+                                <Pressable style={styles.styleIcon} onPress={() => setNewPassword(null)}>
+                                    <Ionicons name="close" size={22} color="gray" />
+                                </Pressable>
+                            ) : null}
+                        </View>
+                        <View style={styles.styleViewGray}>
+                            <TextInput style={[styles.styleText, styles.styleColorGray, { width: '90%' }]}
+                                value={newPassword2}
+                                onChangeText={setNewPassword2}
+                                placeholder="Nhập lại mật khẩu mới"
+                                secureTextEntry={!showPassword}
+                            ></TextInput>
+                            {newPassword2 ? (
+                                <Pressable style={styles.styleIcon} onPress={() => setNewPassword2(null)}>
+                                    <Ionicons name="close" size={22} color="gray" />
+                                </Pressable>
+                            ) : null}
+                        </View>
+                    </View>
+                    <Pressable style={[styles.styleButton, styles.styleCenter, { margin: 30 }]} onPress={handleUpdatePassword}>
+                        <Text style={[styles.styleText, { color: 'white', fontWeight: 'bold' }]}>Cập nhật</Text>
+                    </Pressable>
                 </View>
-                <View style={styles.styleViewGray}>
-                    <TextInput style={[styles.styleText, styles.styleColorGray, { width: '90%' }]}
-                        value={oldPassword}
-                        onChangeText={setOldPassword}
-                        placeholder="Nhập mật khẩu hiện tại"
-                        secureTextEntry={!showPassword}
-                    ></TextInput>
-                    {oldPassword ? (
-                        <Pressable style={styles.styleIcon} onPress={() => setOldPassword(null)}>
-                            <Ionicons name="close" size={22} color="gray" />
-                        </Pressable>) : null}
-                </View>
-                <View style={styles.styleViewBlue}>
-                    <Text style={[styles.styleText, styles.styleColorBlue]}>Mật khẩu mới</Text>
-                </View>
-                <View style={styles.styleViewGray}>
-                    <TextInput style={[styles.styleText, styles.styleColorGray, { width: '90%' }]}
-                        value={newPassword}
-                        onChangeText={setNewPassword}
-                        placeholder="Nhập mật khẩu mới"
-                        secureTextEntry={!showPassword}
-                    ></TextInput>
-                    {newPassword ? (
-                        <Pressable style={styles.styleIcon} onPress={() => setNewPassword(null)}>
-                            <Ionicons name="close" size={22} color="gray" />
-                        </Pressable>
-                    ) : null}
-                </View>
-                <View style={styles.styleViewGray}>
-                    <TextInput style={[styles.styleText, styles.styleColorGray, { width: '90%' }]}
-                        value={newPassword2}
-                        onChangeText={setNewPassword2}
-                        placeholder="Nhập lại mật khẩu mới"
-                        secureTextEntry={!showPassword}
-                    ></TextInput>
-                    {newPassword2 ? (
-                        <Pressable style={styles.styleIcon} onPress={() => setNewPassword2(null)}>
-                            <Ionicons name="close" size={22} color="gray" />
-                        </Pressable>
-                    ) : null}
-                </View>
+            </ScrollView>
+            <View>
+                {/* Modal xác nhận  */}
+                <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={modalSuccess}
+                    onRequestClose={() => { setModalSuccess(!modalSuccess) }}
+                >
+                    <View style={styles.modalContainer}>
+                        <View style={styles.modalSuccess}>
+                            <View style={{ flex: 1, justifyContent: 'center' }}>
+                                <View style={{ alignItems: 'center', paddingHorizontal: 20 }}>
+                                    <View>
+                                        <Ionicons name={"checkmark-circle-sharp"} size={80} color="#00ce3a" style={{ marginRight: 8 }} />
+                                    </View>
+                                    <Text style={styles.modalHeaderText}>Đổi mật khẩu thành công!</Text>
+                                    <Text style={styles.modalText}>
+                                        Vui lòng đăng nhập lại
+                                    </Text>
+
+                                    <View style={styles.modalButtonContainer}>
+                                        <Pressable style={styles.buttonCreatePass} onPress={handleLogout}>
+                                            <Text style={styles.modalButton}>OK</Text>
+                                        </Pressable>
+                                    </View>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
             </View>
-            <Pressable style={[styles.styleButton, styles.styleCenter, { margin: 30 }]} onPress={handleUpdatePassword}>
-                <Text style={[styles.styleText, { color: 'white', fontWeight: 'bold' }]}>Cập nhật</Text>
-            </Pressable>
         </View>
 
     );
@@ -192,5 +238,48 @@ const styles = {
     toastContainer: {
         zIndex: 99,
     },
-
+    modalContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(0,0,0,0.5)",
+    },
+    modalSuccess: {
+        backgroundColor: "#fff",
+        width: '80%',
+        height: '40%',
+        borderRadius: 10,
+    },
+    modalHeaderText: {
+        fontWeight: "bold",
+        marginBottom: 10,
+        textAlign: "center",
+        fontSize: 18,
+        marginVertical: 10
+    },
+    modalText: {
+        marginVertical: 20,
+        marginHorizontal: 10,
+        fontSize: 16,
+        textAlign: 'center',
+    },
+    modalButtonContainer: {
+        justifyContent: "flex-end",
+        width: '80%',
+        alignItems: 'center'
+    },
+    buttonCreatePass: {
+        width: '80%',
+        height: 45,
+        backgroundColor: '#0091FF',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    modalButton: {
+        fontWeight: "bold",
+        marginHorizontal: 10,
+        color: "white",
+        fontSize: 16
+    },
 };
