48fa68b1739029b2336b982c0356c4e3cd346cc4
BaoTruc <tranbaotruc2002bc@gmail.com>
2024-04-06 01:00:26 +0700

    Update Mobile: Show message when clicking on your friend

--- Frontend/Mobile/zalofake/src/components/ChatComponents/message/Message.js
+++ Frontend/Mobile/zalofake/src/components/ChatComponents/message/Message.js
@@ -1,15 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
   TouchableOpacity,
   TextInput,
   Pressable,
+  StyleSheet
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import axiosInstance from "../../../api/axiosInstance";
+import { useAuthContext } from "../../../contexts/AuthContext";
 
 const Message = ({ navigation, route }) => {
   const { user } = route.params;
+  const [chats, setChats] = useState([]);
+  const [isSend, setIsSend] = useState("");
+
+  useEffect(() => {
+    const fetchChats = async () => {
+      try {
+        const response = await axiosInstance.get(`/chats/${user.userId}`);
+        const reversedChats = response.data.data.reverse(); 
+        setChats(reversedChats);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchChats();
+  }, []);
+
+  const handleCheckIsSend = (message) => {
+    if(message.senderId===user.userId){
+      // setIsSend(false);
+      return false;
+    }else{
+      return true;
+      // setIsSend(true);
+    }
+  };
+  const handleGetTime = (time) => {
+    const dateObject = new Date(time); // Chuyển đổi chuỗi thành đối tượng Date
+    return `${dateObject.getUTCHours()}:${dateObject.getUTCMinutes()}`
+  };
+
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
@@ -42,7 +75,7 @@ const Message = ({ navigation, route }) => {
       ),
       headerTitle: () => (
         <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <Text style={{ fontSize: 20 }}>{user.ten}</Text>
+          <Text style={{ fontSize: 20 }}>{user.profile.name}</Text>
         </View>
       ),
       headerStyle: {
@@ -57,10 +90,24 @@ const Message = ({ navigation, route }) => {
     });
   }, [navigation]);
 
+
+
   return (
     <View style={{ flex: 1, backgroundColor: "#E5E9EB" }}>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>message</Text>
+      <View style={{ flex: 1, justifyContent: "center" }}>
+       
+        <View style={{flex: 1, justifyContent: "flex-start" }}>
+        {chats.map((message, index) => (
+          <View key={index} style={{width:'75%', justifyContent:'space-around', borderRadius:10,backgroundColor: handleCheckIsSend(message) ? "#7debf5" : "#d9d9d9", margin:5,alignItems: handleCheckIsSend(message) ? "flex-end" :"flex-start", alignSelf: handleCheckIsSend(message) ? "flex-end" :"flex-start"}}>        
+              <View style={{paddingLeft:10, paddingRight:10, paddingTop:5}} >
+               <Text style={{ fontSize:18}}>{message.contents[0].data}</Text>
+                
+              </View>
+              <View style={{paddingLeft:10, paddingRight:10, paddingBottom:5}}><Text style={{ fontSize:18}}>{handleGetTime(message.timestamp)}</Text></View>
+          </View>
+        ))}
+        </View>
+
       </View>
 
       <View
@@ -114,5 +161,107 @@ const Message = ({ navigation, route }) => {
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+  },
+  topSection: {
+    backgroundColor: "white",
+    padding: 10,
+  },
+  buttonRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 5,
+  },
+  buttonText: {
+    marginLeft: 10,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  iconImage: {
+    width: 40,
+    height: 40,
+    marginRight: 10,
+  },
+  buttonBar: {
+    flexDirection: "row",
+    backgroundColor: "white",
+    padding: 10,
+    marginTop: 10,
+  },
+  roundedButton: {
+    borderRadius: 50,
+    backgroundColor: "#bebebe",
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+  borderedButton: {
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: "#e5e5e5",
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+  },
+  whiteText: {
+    color: "white",
+    fontSize: 16,
+  },
+  grayText: {
+    color: "gray",
+    fontSize: 16,
+  },
+  friendList: {
+    backgroundColor: "white",
+    padding: 10,
+    borderTopColor: "#e5e5e5",
+    borderTopWidth: 0.5,
+  },
+  friendListHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  titleRow: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 5,
+  },
+  addText: {
+    color: "#0091FF",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  friendRow: {
+    paddingVertical: 10,
+  },
+  friendItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  friendInfo: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  friendAvatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  friendName: {
+    fontSize: 16,
+  },
+  friendActions: {
+    flexDirection: "row",
+  },
+  actionIcon: {
+    marginRight: 20,
+  },
+});
 export default Message;
