fce1a797cabd52161c33c9f1f45d330d0105e528
NguyenVanLoc2002 <nguyenvanloc6756@gmail.com>
2024-04-08 03:32:42 +0700

    ud new

--- Backend/models/Conversation.js
+++ Backend/models/Conversation.js
@@ -6,8 +6,11 @@ const ConversationSchema = new mongoose.Schema({
   ],
   messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "chats" }],
   createdAt: { type: Date, default: Date.now },
-  tag: { type: String,default: "friend", optional: true },
+  tag: { type: String, default: "friend", optional: true },
 });
+
+// Loại bỏ chỉ mục duy nhất cho trường participants
+// ConversationSchema.index({ participants: 1 }, { unique: false });
 
 const Conversation = mongoose.model("Conversation", ConversationSchema);
 
