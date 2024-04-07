0a95e0850bc72ebdda3a17a59152f1173385978d
BaoTruc <tranbaotruc2002bc@gmail.com>
2024-04-07 12:37:19 +0700

    Update mobile: chuyen tiep 1 text

---
+++ Frontend/Mobile/zalofake/src/hooks/useSendMessage.js
@@ -0,0 +1,33 @@
+import axiosInstance from "../api/axiosInstance";
+
+const useSendMessage = () => {
+
+    const sendMessage = async (user, message) => {
+        console.log("message: ", message);
+        try {
+            const response = await axiosInstance.post(`/chats/${user.userId}/sendMessage`,
+                {
+                    data: message,
+                   
+                }
+
+            )
+            if (response.status === 201) {
+                console.log("success");
+                return true;
+            }
+            else if (response.status === 500) {
+                console.log("fail");
+                return false;
+            }
+
+        } catch (error) {
+            console.log("error1:", error)
+            return false;
+        }
+    }
+
+    return { sendMessage }
+}
+
+export default useSendMessage;
