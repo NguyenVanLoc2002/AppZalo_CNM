5cd684354b2c2f0739ecd29eac2417eeac2f7889
Bùi Trí Thức <20088361.thuc@student.iuh.edu.vn>
2024-04-08 13:43:55 +0700

    Update feature : Unfriend and delete conversation

--- Backend/models/Conversation.js
+++ Backend/models/Conversation.js
@@ -1,14 +1,31 @@
 const mongoose = require("mongoose");
 
 const ConversationSchema = new mongoose.Schema({
-  participants: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "users" },
-  ],
+  participants: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
+    required: true,
+    unique: false,
+    index: false,
+  },
   messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "chats" }],
+  lastMessage: { type: mongoose.Schema.Types.ObjectId, ref: "chats" },
   createdAt: { type: Date, default: Date.now },
-  tag: { type: String,default: "friend", optional: true },
+  tag: { type: String, default: "friend", optional: true },
 });
 
 const Conversation = mongoose.model("Conversation", ConversationSchema);
+
+ConversationSchema.pre("deleteOne", async function (next) {
+  try {
+    const Chat = mongoose.model("chats");
+    const messagesToDelete = await Chat.find({ _id: { $in: this.messages } });
+    for (const message of messagesToDelete) {
+      await message.deleteOne();
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = Conversation;
