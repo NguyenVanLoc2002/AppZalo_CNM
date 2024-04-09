c84aeb0f1b437d37444c3e1aa7ee5dd513832503
yennhii134 <yennhi13402@gmail.com>
2024-04-10 06:48:52 +0700

    Update Moblie: update socket

--- Frontend/Mobile/zalofake/src/api/config.js
+++ Frontend/Mobile/zalofake/src/api/config.js
@@ -1,6 +1,7 @@
 // config.js
 const config = {
-  baseURL: "http://192.168.1.2:3000/api", // Thay đổi địa chỉ IP của máy chủ tại đây
+  baseURL: "http://192.168.20.111:3000/api", // Thay đổi địa chỉ IP của máy chủ tại đây
+  socketURL:  "http://192.168.20.111:3000"
 };
 
 export default config;
