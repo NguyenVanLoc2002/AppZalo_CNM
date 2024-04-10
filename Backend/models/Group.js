92946dc18c7ffb651ee3d976f686e6332d4f4998
NguyenVanLoc2002 <nguyenvanloc6756@gmail.com>
2024-04-10 12:00:38 +0700

    cm

--- Backend/models/Group.js
+++ Backend/models/Group.js
@@ -2,7 +2,19 @@ const mongoose = require("mongoose");
 
 const groupSchema = new mongoose.Schema({
   groupName: { type: String, required: true },
-  member: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
+  member: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      unique: false,
+      index: false,
+      required: true,
+      ref: "users",
+    },
+  ],
+  avatar: {
+    url: { type: String },
+    public_id: { type: String },
+  },
   createAt: { type: Date, default: Date.now() },
 });
 
