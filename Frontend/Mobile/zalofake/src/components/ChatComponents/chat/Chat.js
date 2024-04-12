16ed03ddc133a6411469875d0bb9efb1350f8ed3
BaoTruc <tranbaotruc2002bc@gmail.com>
2024-04-12 16:04:22 +0700

    update mobile: load message for conversation

--- Frontend/Mobile/zalofake/src/components/ChatComponents/chat/Chat.js
+++ Frontend/Mobile/zalofake/src/components/ChatComponents/chat/Chat.js
@@ -14,12 +14,16 @@ import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import axiosInstance from "../../../api/axiosInstance";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import moment from 'moment-timezone';
+import { useAuthContext } from "../../../contexts/AuthContext";
+import { useSocketContext } from "../../../contexts/SocketContext"
 
 function Chat({ navigation }) {
   const [isModalVisible, setModalVisible] = useState(false);
-  // const [friends, setFriends] = useState([]);
+  const [conversations, setConversations] = useState([]);
   const [listFriends, setListFriends] = useState([]);
   const [isLoad, SetIsLoad] = useState(false);
+  const { authUser } = useAuthContext();
+  const { socket } = useSocketContext()
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
@@ -75,16 +79,79 @@ function Chat({ navigation }) {
     });
   }, [navigation]);
 
-
-  const fetchData = async () => {
+  const fetchDataConver = async () => {
     try {
       const AsyncStorageValue = await AsyncStorage.getAllKeys();
-      console.log("AsyncStorageValue: ", AsyncStorageValue);
-      const response = await axiosInstance.get("/users/get/friends");
+      // console.log("AsyncStorageValue: ", AsyncStorageValue);
+      const response = await axiosInstance.get("/conversations/getConversations");
       if (response.status === 200) {
-        // setFriends(response.data.friends);
-        fetchDataFriend(response.data.friends)
-        // console.log(response.data.friends)
+        // console.log(response.data)
+        let data = [];
+        let name;
+        let image;
+        let dataChat;
+        let user;
+        for (let index = 0; index < response.data.length; index++) {
+          const conver = response.data[index];
+          // console.log(authUser.phone)
+          if (conver.lastMessage !== undefined) {
+            if(conver.participants.length!=2){
+              for(let index = 0; index < conver.participants.length; index++){
+                const part = conver.participants[index];
+                name = name + part.profile?.name;
+                user = conver;
+              };
+              image = "https://fptshop.com.vn/Uploads/Originals/2021/6/23/637600835869525914_thumb_750x500.png"
+            }else{
+              if(conver.participants[0].phone===authUser.phone){
+                image = conver.participants[1].profile?.avatar?.url;
+                name = conver.participants[1].profile?.name;
+                user = conver.participants[1];
+                if (conver.participants[1]._id === conver.lastMessage.senderId) {
+                  dataChat = conver.participants[1].profile?.name;
+                } else {
+                  dataChat = 'Bạn'
+                }
+                if (conver.lastMessage.contents[0].type === "text") {
+                  dataChat = dataChat + ': ' + conver.lastMessage.contents[0].data;
+                } else if (conver.lastMessage.contents[0].type === "image") {
+                  dataChat = dataChat + ': [Hình ảnh]';
+                } else {
+                  dataChat = dataChat + ': [Video]';
+                }
+              }else{
+                image = conver.participants[0].profile?.avatar?.url;
+                name = conver.participants[0].profile?.name;
+                user = conver.participants[0];
+                if (conver.participants[0]._id === conver.lastMessage.senderId) {
+                  dataChat = conver.participants[0].profile?.name;
+                } else {
+                  dataChat = 'Bạn'
+                }
+                if (conver.lastMessage.contents[0].type === "text") {
+                  dataChat = dataChat + ': ' + conver.lastMessage.contents[0].data;
+                } else if (conver.lastMessage.contents[0].type === "image") {
+                  dataChat = dataChat + ': [Hình ảnh]';
+                } else {
+                  dataChat = dataChat + ': [Video]';
+                }
+              }
+            }
+            
+            const conversationNew = {
+              conver:conver,
+              name: name,
+              image:image,
+              dataChat:dataChat,
+              time: handleGetTime(conver.lastMessage.timestamp),
+              user:user
+            };
+            data.push(conversationNew);
+          }
+        };
+        data.sort((a, b) => b.conver.lastMessage.timestamp - a.conver.lastMessage.timestamp);
+        setConversations(data)
+        console.log(data)
       }
       else if (response.status === 404) {
         console.log("getFriendError:", error);
@@ -93,61 +160,81 @@ function Chat({ navigation }) {
       console.log("getFriendError:", error);
     }
   };
-  const fetchDataFriend = async (fr) => {
-    const idSet = new Set();
-    for (let index = 0; index < fr.length; index++) {
-      const friend = fr[index];
-      try {
-        const getChat = await axiosInstance.get(`/chats/${friend.userId}/getLastMessage`);
-        if (getChat.status === 200) {
-          let dataChat;
-          console.log(friend.userId)
-          console.log(getChat.data.data.senderId)
-          if(friend.userId === getChat.data.data.senderId){
-            dataChat = friend.profile?.name ;
-          }else{
-            dataChat = 'Bạn'
-          }
-          if (getChat.data.data.contents[0].type === "text") {
-            dataChat = dataChat + ': ' +getChat.data.data.contents[0].data;
-          } else if (getChat.data.data.contents[0].type === "image") {
-            dataChat =  dataChat +': [Hình ảnh]';
-          } else {
-            dataChat = dataChat +': [Video]';
-          }
-          
-          const newFriend = {
-            friend: friend,
-            chat: dataChat,
-            time: handleGetTime(getChat.data.data.timestamp)
-          };
-          if (!idSet.has(friend.userId)) {
-            setListFriends(prevList => [...prevList, newFriend]);
-            idSet.add(friend.userId);
-          }
-        } else {
-          console.log("Error get chat");
-        }
-      } catch (error) {
-        console.log("Error:", error);
-      }
-    }
-  }
+  
+
+  // const fetchData = async () => {
+  //   try {
+  //     const AsyncStorageValue = await AsyncStorage.getAllKeys();
+  //     console.log("AsyncStorageValue: ", AsyncStorageValue);
+  //     const response = await axiosInstance.get("/users/get/friends");
+  //     if (response.status === 200) {
+  //       // setFriends(response.data.friends);
+  //       fetchDataFriend(response.data.friends)
+  //       // console.log(response.data.friends)
+  //     }
+  //     else if (response.status === 404) {
+  //       console.log("getFriendError:", error);
+  //     }
+  //   } catch (error) {
+  //     console.log("getFriendError:", error);
+  //   }
+  // };
+  // const fetchDataFriend = async (fr) => {
+  //   const idSet = new Set();
+  //   for (let index = 0; index < fr.length; index++) {
+  //     const friend = fr[index];
+  //     try {
+  //       const getChat = await axiosInstance.get(`/chats/${friend.userId}/getLastMessage`);
+  //       if (getChat.status === 200) {
+  //         let dataChat;
+  //         console.log(friend.userId)
+  //         console.log(getChat.data.data.senderId)
+  //         if (friend.userId === getChat.data.data.senderId) {
+  //           dataChat = friend.profile?.name;
+  //         } else {
+  //           dataChat = 'Bạn'
+  //         }
+  //         if (getChat.data.data.contents[0].type === "text") {
+  //           dataChat = dataChat + ': ' + getChat.data.data.contents[0].data;
+  //         } else if (getChat.data.data.contents[0].type === "image") {
+  //           dataChat = dataChat + ': [Hình ảnh]';
+  //         } else {
+  //           dataChat = dataChat + ': [Video]';
+  //         }
+
+  //         const newFriend = {
+  //           friend: friend,
+  //           chat: dataChat,
+  //           time: handleGetTime(getChat.data.data.timestamp)
+  //         };
+  //         if (!idSet.has(friend.userId)) {
+  //           setListFriends(prevList => [...prevList, newFriend]);
+  //           idSet.add(friend.userId);
+  //         }
+  //       } else {
+  //         console.log("Error get chat");
+  //       }
+  //     } catch (error) {
+  //       console.log("Error:", error);
+  //     }
+  //   }
+  // }
   useEffect(() => {
     const fetchDataListFriend = async () => {
       try {
-        await fetchData();
+        await fetchDataConver();
       } catch (error) {
         console.log("getFriendError:", error);
       }
     };
-    if(!isLoad){
+    if (!isLoad) {
       fetchDataListFriend();
       SetIsLoad(true);
     }
   }, []);
   const handleChatItemPress = (item) => {
-    navigation.navigate("Message", { user: item.friend });
+    // fetchDataConver();
+    navigation.navigate("Message", { conver: item.conver , user: item.user});
   };
   const handleGetTime = (time) => {
     const currentTime = moment().tz('Asia/Ho_Chi_Minh'); // Lấy thời gian hiện tại ở múi giờ Việt Nam
@@ -159,26 +246,26 @@ function Chat({ navigation }) {
     const minutes = Math.abs(timeDifference.minutes()); // Số phút (dương)
 
     if (days >= 1) {
-        return `${days} ngày`;
+      return `${days} ngày`;
     }
-    else if(hours>=1){
+    else if (hours >= 1) {
       return `${hours} giờ`;
     }
     else {
-        return `${minutes} phút`;
+      return `${minutes} phút`;
     }
   };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FlatList
-        data={listFriends}
+        data={conversations}
         renderItem={({ item }) => (
           <Pressable onPress={() => handleChatItemPress(item)} >
-            <ChatItem item={item}  />
+            <ChatItem item={item} />
           </Pressable>
         )}
-        keyExtractor={(item) => item.url}
+        keyExtractor={(item) => item.conver._id}
       />
       <Modal
         animationType="none"
