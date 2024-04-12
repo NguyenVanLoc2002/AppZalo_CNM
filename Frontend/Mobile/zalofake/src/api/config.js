48e9bbb56c3beac67ca230cb0b21ca6485b73700
BaoTruc <tranbaotruc2002bc@gmail.com>
2024-04-12 22:37:44 +0700

    update mobile: fix config of me

--- Frontend/Mobile/zalofake/src/api/config.js
+++ Frontend/Mobile/zalofake/src/api/config.js
@@ -1,7 +1,7 @@
 // config.js
 const config = {
-  baseURL: "http://192.168.137.239:3000/api", // Thay đổi địa chỉ IP của máy chủ tại đây
-  socketURL:  "http://192.168.137.239:3000"
+  baseURL: "http://192.168.3.6:3000/api", // Thay đổi địa chỉ IP của máy chủ tại đây
+  socketURL:  "http://192.168.3.6:3000"
 };
 
 export default config;
