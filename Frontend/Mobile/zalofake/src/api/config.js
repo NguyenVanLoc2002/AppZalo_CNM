1d7cee0da4aab89ee1d7faa2737646bf96bc5f0d
Bùi Trí Thức <20088361.thuc@student.iuh.edu.vn>
2024-04-19 02:24:17 +0700

    uninstall rn-reaser-pdf

--- Frontend/Mobile/zalofake/src/api/config.js
+++ Frontend/Mobile/zalofake/src/api/config.js
@@ -1,7 +1,9 @@
 // config.js
 const config = {
-  baseURL: "http://192.168.3.6:3000/api", // Thay đổi địa chỉ IP của máy chủ tại đây
-  socketURL:  "http://192.168.3.6:3000"
+  baseURL: "http://18.143.74.162/api",
+  socketURL:  "http://18.143.74.162/"
+  // baseURL: "http://192.168.3.6:3000/api",
+  // socketURL:  "http://192.168.3.6:3000"
 };
 
 export default config;
