1882c59ce2ce51a5b46fd1b3f20a7909f0536266
TriThuc1506 <trithucbuittb@gmail.com>
2024-05-12 00:55:15 +0700

    Update BE : Add feature get group by participants ( get common group between user and friend ). Add Feature make member to admin of group. Update FE Web :Add Feature make member to admin of group.

--- Frontend/Mobile/zalofake/src/api/axiosInstance.js
+++ Frontend/Mobile/zalofake/src/api/axiosInstance.js
@@ -10,10 +10,10 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   async (config) => {
     try {
-      config.headers["User-Agent"] +=  "Mobile"
+      config.headers["User-Agent"] += "Mobile";
 
       if (!config.url.includes("/auth/login")) {
-        const token = JSON.parse( await AsyncStorage.getItem("accessToken"));
+        const token = JSON.parse(await AsyncStorage.getItem("accessToken"));
         if (token) {
           config.headers.Authorization = `Bearer ${token}`;
         }
@@ -42,24 +42,25 @@ axiosInstance.interceptors.response.use(
         const refreshToken = JSON.parse(
           await AsyncStorage.getItem("refreshToken")
         );
-        console.log("refreshToken", refreshToken);
-        if (!refreshToken) {
-          showErrorToast("Your session has expired. Please login again.");
-          throw new Error("No refresh token available.");
-        }
 
-        const refreshedTokenResponse = await axiosInstance.post(
-          "/auth/refreshToken",
-          {
-            refreshToken: refreshToken,
-          }
-        );
+        Promise.all([refreshToken]).then(async (values) => {
+          const refreshedTokenResponse = await axiosInstance.post(
+            "/auth/refreshToken",
+            {
+              refreshToken: values[0],
+            }
+          );
+  
+          const newAccessToken = refreshedTokenResponse.data.newAccessToken;
+          await AsyncStorage.setItem(
+            "accessToken",
+            JSON.stringify(newAccessToken)
+          );
+  
+          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+          return axiosInstance(originalRequest);
+        });
 
-        const newAccessToken = refreshedTokenResponse.data.newAccessToken;
-        await AsyncStorage.setItem("accessToken", JSON.stringify(newAccessToken));
-
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-        return axiosInstance(originalRequest);
       } catch (refreshError) {
         // console.error("Refresh token failed:", refreshError);
         showErrorToast("Your session has expired. Please login again.");
