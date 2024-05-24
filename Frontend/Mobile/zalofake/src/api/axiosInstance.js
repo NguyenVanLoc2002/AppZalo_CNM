455cb0909b9f33e278cbaba8421e2d3e6ac308b9
TriThuc1506 <trithucbuittb@gmail.com>
2024-05-25 01:53:31 +0700

    fix axios interceptor on mobile

--- Frontend/Mobile/zalofake/src/api/axiosInstance.js
+++ Frontend/Mobile/zalofake/src/api/axiosInstance.js
@@ -13,9 +13,11 @@ axiosInstance.interceptors.request.use(
       config.headers["User-Agent"] += "Mobile";
 
       if (!config.url.includes("/auth/login")) {
-        const token = JSON.parse(await AsyncStorage.getItem("accessToken"));
-        if (token) {
-          config.headers.Authorization = `Bearer ${token}`;
+        const accessToken = JSON.parse(
+          await AsyncStorage.getItem("accessToken")
+        );
+        if (accessToken) {
+          config.headers.Authorization = `Bearer ${accessToken}`;
         }
       }
       return config;
@@ -43,24 +45,25 @@ axiosInstance.interceptors.response.use(
           await AsyncStorage.getItem("refreshToken")
         );
 
-        Promise.all([refreshToken]).then(async (values) => {
-          const refreshedTokenResponse = await axiosInstance.post(
-            "/auth/refreshToken",
-            {
-              refreshToken: values[0],
-            }
-          );
-  
-          const newAccessToken = refreshedTokenResponse.data.newAccessToken;
+        const refreshedTokenResponse = await axiosInstance.post(
+          "/auth/refreshToken",
+          {
+            refreshToken,
+          }
+        );
+        const newAccessToken = refreshedTokenResponse.data.newAccessToken;
+        if (!newAccessToken) {
+          showErrorToast("Your session has expired. Please login again.");
+          await AsyncStorage.clear();
+          return Promise.reject(error);
+        } else {
           await AsyncStorage.setItem(
             "accessToken",
             JSON.stringify(newAccessToken)
           );
-  
-          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-          return axiosInstance(originalRequest);
-        });
-
+        }
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return axiosInstance(originalRequest);
       } catch (refreshError) {
         // console.error("Refresh token failed:", refreshError);
         showErrorToast("Your session has expired. Please login again.");
--- Frontend/Mobile/zalofake/src/api/config.js
+++ Frontend/Mobile/zalofake/src/api/config.js
@@ -1,9 +1,9 @@
 // config.js
 const config = {
-  baseURL: "http://192.168.1.6:3000/api", // Thay đổi địa chỉ IP của máy chủ tại đây
-  socketURL:  "http://192.168.1.6:3000"
-  // baseURL: "https://zolachat.io.vn/api", // Thay đổi địa chỉ IP của máy chủ tại đây
-  // socketURL:  "https://zolachat.io.vn"
+  // baseURL: "http://192.168.1.6:3000/api", // Thay đổi địa chỉ IP của máy chủ tại đây
+  // socketURL:  "http://192.168.1.6:3000"
+  baseURL: "https://zolachat.io.vn/api", // Thay đổi địa chỉ IP của máy chủ tại đây
+  socketURL:  "https://zolachat.io.vn"
 };
 
 export default config;
